10be203cb57072e2e0121222b2600dcfc8a92e1f
Keith Stanger <kstanger@google.com>
2012-11-19 09:14:31 -0500

    Account for unicode character \ud800 to fix translation of CharMatcher.java. 	Change on 2012/11/06 by pankaj@pankaj ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=38178729

--- src/main/java/com/google/devtools/j2objc/util/UnicodeUtils.java
+++ src/main/java/com/google/devtools/j2objc/util/UnicodeUtils.java
@@ -141,7 +141,7 @@ public final class UnicodeUtils {
   public static boolean isValidCppCharacter(char c) {
     // This would be more efficiently implemented as a bitmap, but since it's
     // not used in performance-critical code, this form is easier to inspect.
-    return c < 0xe000 ||
+    return c < 0xd800 ||
         c >= 0xf900 && c <= 0xfad9 ||
         c >= 0xfb50 && c <= 0xfbc1 ||
         c >= 0xfbd3 && c <= 0xfd3f ||
