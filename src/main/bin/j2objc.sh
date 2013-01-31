25d87d4aa7762d291a34a36fe582bd77ad5af0b9
Tom Ball <tball@google.com>
2013-01-31 11:36:35 -0800

    Added -begin-java-args and -end-java-args to j2objc.sh.
    The previous attempt to pass arguments to java filtered out arguments beginning with -X. This approach doesn't always work because:
    1. there are some java arguments that don't begin with -X
    2. there are some j2objc arguments that do begin with -X
    	Change on 2013/01/29 by iroth <iroth@google.com>
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=41810042

--- src/main/bin/j2objc.sh
+++ src/main/bin/j2objc.sh
@@ -4,7 +4,12 @@
 # Run j2objc translation tool.
 #
 # Usage:
-#   j2objc [java_options] [-classpath path] [-sourcepath path] [-d outputDirectory] <file.java> ...
+#   j2objc
+#     [-begin-java-args <java-options> -end-java-args]
+#     [-classpath <path>]
+#     [-sourcepath <path>]
+#     [-d <output-directory>]
+#     <file.java> ...
 #
 
 if [ -L "$0" ]; then
@@ -24,14 +29,20 @@ if [ x${USE_SYSTEM_BOOT_PATH} == x ]; then
   readonly BOOT_PATH=-Xbootclasspath:${LIB_DIR}/jre_emul.jar
 fi
 
-# Java arguments beginning with -X must go first in the argument list.
+PARSING_JAVA_ARGS=0
 JAVA_ARGS=""
 J2OBJC_ARGS=""
 for ARG in $@; do
-  if [[ ${ARG} == -X* ]]; then
-    JAVA_ARGS="${JAVA_ARGS} ${ARG}"
+  if [ "${ARG}" == "-begin-java-args" ]; then
+    PARSING_JAVA_ARGS=1
+  elif [ "${ARG}" == "-end-java-args" ]; then
+    PARSING_JAVA_ARGS=0
   else
-    J2OBJC_ARGS="${J2OBJC_ARGS} ${ARG}"
+    if [ ${PARSING_JAVA_ARGS} -eq 0 ]; then
+      J2OBJC_ARGS="${J2OBJC_ARGS} ${ARG}"
+    else
+      JAVA_ARGS="${JAVA_ARGS} ${ARG}"
+    fi
   fi
 done
 
