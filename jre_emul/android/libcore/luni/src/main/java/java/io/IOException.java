ac1dcacd399ebbdde959232a31e06766690f2b13
Tom Ball <tball@google.com>
2013-02-19 15:36:59 -0800

    Updated IOException to Android version, that has new 1.6 constructors. 	Change on 2013/02/19 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=42778188

--- jre_emul/android/libcore/luni/src/main/java/java/io/IOException.java
+++ jre_emul/android/libcore/luni/src/main/java/java/io/IOException.java
@@ -25,21 +25,47 @@ package java.io;
  */
 public class IOException extends Exception {
 
+    private static final long serialVersionUID = 7818375828146090155L;
+
     /**
      * Constructs a new {@code IOException} with its stack trace filled in.
      */
     public IOException() {
-        super();
     }
 
     /**
      * Constructs a new {@code IOException} with its stack trace and detail
      * message filled in.
-     * 
+     *
      * @param detailMessage
      *            the detail message for this exception.
      */
     public IOException(String detailMessage) {
         super(detailMessage);
+    }
+
+    /**
+     * Constructs a new instance of this class with detail message and cause
+     * filled in.
+     *
+     * @param message
+     *            The detail message for the exception.
+     * @param cause
+     *            The detail cause for the exception.
+     * @since 1.6
+     */
+    public IOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new instance of this class with its detail cause filled in.
+     *
+     * @param cause
+     *            The detail cause for the exception.
+     * @since 1.6
+     */
+    public IOException(Throwable cause) {
+        super(cause == null ? null : cause.toString(), cause);
     }
 }
