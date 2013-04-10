a49bc40ce1cf04193bdf017e92302c8ea80578b3
Tom Ball <tball@google.com>
2013-04-10 15:28:42 -0700

    Add new files to opensource filegroup. 	Change on 2013/03/08 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=43620415

---
+++ annotations/src/main/java/com/google/j2objc/annotations/AutoreleasePool.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.j2objc.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation that indicates the translator should inject an autorelease pool
+ * around the method body. Only valid on methods that don't return anything.
+ *
+ * @author Pankaj Kakkar
+ */
+@Target({ElementType.METHOD, ElementType.LOCAL_VARIABLE})
+@Retention(RetentionPolicy.CLASS)
+public @interface AutoreleasePool {
+}
