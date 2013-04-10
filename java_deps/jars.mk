bded1047753813c28375b9e77315bb7385b43224
Tom Ball <tball@google.com>
2013-04-10 15:28:42 -0700

    Add new files to opensource filegroup. 	Change on 2013/03/08 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=43620415

---
+++ java_deps/jars.mk
@@ -0,0 +1,32 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Defines the names of all dependent .jar files used by j2objc.
+#
+# Author: Keith Stanger
+
+ECLIPSE_JARS = \
+    org.eclipse.core.contenttype-3.4.100.v20100505-1235.jar \
+    org.eclipse.core.jobs-3.5.0.v20100515.jar \
+    org.eclipse.core.resources-3.6.0.v20100526-0737.jar \
+    org.eclipse.core.runtime-3.6.0.v20100505.jar \
+    org.eclipse.equinox.common-3.6.0.v20100503.jar \
+    org.eclipse.equinox.preferences-3.3.0.v20100503.jar \
+    org.eclipse.jdt.core-3.8.1.v20120531-0637.jar \
+    org.eclipse.osgi-3.6.0.v20100517.jar \
+    org.eclipse.text-3.5.100.v20110505-0800.jar
+
+GUAVA_JAR = guava-13.0.jar
+
+JSR305_JAR = jsr305-2.0.1.jar
+
+JUNIT_JAR = junit-4.10.jar
