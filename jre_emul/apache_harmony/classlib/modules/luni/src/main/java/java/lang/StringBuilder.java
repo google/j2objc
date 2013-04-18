72cdf58eaff19d49144e511f1f045c473ce2ba22
Tom Ball <tball@google.com>
2013-04-17 17:12:05 -0700

    Added appendCodePoint() to StringBuffer and StringBuilder. 	Change on 2013/04/15 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=45313073

--- jre_emul/apache_harmony/classlib/modules/luni/src/main/java/java/lang/StringBuilder.java
+++ jre_emul/apache_harmony/classlib/modules/luni/src/main/java/java/lang/StringBuilder.java
@@ -331,6 +331,20 @@ public final class StringBuilder implements Appendable, CharSequence, Serializab
     }
 
     /**
+     * Appends the encoded Unicode code point. The code point is converted to a
+     * {@code char[]} as defined by {@link Character#toChars(int)}.
+     *
+     * @param codePoint
+     *            the Unicode code point to encode and append.
+     * @return this builder.
+     * @see Character#toChars(int)
+     */
+    public StringBuilder appendCodePoint(int codePoint) {
+        append0(Character.toChars(codePoint));
+        return this;
+    }
+
+    /**
      * Deletes a sequence of characters specified by {@code start} and {@code
      * end}. Shifts any remaining characters to the left.
      * 
