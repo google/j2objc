51d1cee6656e09ecccf45c137abaa50269dc0ee7
Tom Ball <tball@google.com>
2013-04-17 17:10:30 -0700

    Added java.lang.AutoCloseable. 	Change on 2013/04/12 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=45219708

--- jre_emul/android/libcore/luni/src/main/java/java/io/Closeable.java
+++ jre_emul/android/libcore/luni/src/main/java/java/io/Closeable.java
@@ -4,9 +4,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,19 +17,17 @@
 package java.io;
 
 /**
- * Defines an interface for classes that can (or need to) be closed once they
- * are not used any longer. This usually includes all sorts of
- * {@link InputStream}s and {@link OutputStream}s. Calling the {@code close}
- * method releases resources that the object holds.
+ * An {@code AutoCloseable} whose close method may throw an {@link IOException}.
  */
-public interface Closeable {
+public interface Closeable extends AutoCloseable {
 
     /**
-     * Closes the object and release any system resources it holds. If the
-     * object has already been closed, then invoking this method has no effect.
-     * 
-     * @throws IOException
-     *             if any error occurs when closing the object.
+     * Closes the object and release any system resources it holds.
+     *
+     * <p>Although only the first call has any effect, it is safe to call close
+     * multiple times on the same object. This is more lenient than the
+     * overridden {@code AutoCloseable.close()}, which may be called at most
+     * once.
      */
-    public void close() throws IOException;
+    void close() throws IOException;
 }
