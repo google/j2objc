67a355e22466e9184ecf4be94686e722f55bc64b
Tom Ball <tball@google.com>
2013-07-31 12:48:51 -0700

    Box String.format primitive args that are referred to as objects in the format string. 	Change on 2013/07/17 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=49515151

--- translator/src/main/java/com/google/devtools/j2objc/translate/Autoboxer.java
+++ translator/src/main/java/com/google/devtools/j2objc/translate/Autoboxer.java
@@ -16,6 +16,7 @@
 
 package com.google.devtools.j2objc.translate;
 
+import com.google.common.collect.Lists;
 import com.google.devtools.j2objc.types.IOSMethodBinding;
 import com.google.devtools.j2objc.types.NodeCopier;
 import com.google.devtools.j2objc.types.Types;
@@ -45,6 +46,7 @@ import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.PrefixExpression;
 import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
 import org.eclipse.jdt.core.dom.SwitchStatement;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
@@ -422,7 +424,31 @@ public class Autoboxer extends ErrorReportingASTVisitor {
     if (Types.isJavaStringType(methodBinding.getDeclaringClass()) &&
         methodBinding.getName().equals("format")) {
       // The String.format methods are mapped directly to NSString methods,
-      // and are already converted by Rewriter.
+      // but arguments referred to as objects by the format string need to
+      // be boxed.
+      if (!args.isEmpty()) {
+        Expression first = args.get(0);
+        int firstArg = 1;
+        ITypeBinding typeBinding = Types.getTypeBinding(first);
+        if (typeBinding.getQualifiedName().equals("java.util.Locale")) {
+          first = args.get(1);
+          firstArg = 2;
+          typeBinding = Types.getTypeBinding(first);
+        }
+        if (first instanceof StringLiteral) {
+          List<Boolean> formatSpecifiers =
+              getFormatSpecifiers(((StringLiteral) first).getLiteralValue());
+          for (int i = 0; i < formatSpecifiers.size(); i++) {
+            if (formatSpecifiers.get(i)) {
+              int iArg = i + firstArg;
+              Expression arg = args.get(iArg);
+              if (Types.getTypeBinding(arg).isPrimitive()) {
+                args.set(iArg, box(arg));
+              }
+            }
+          }
+        }
+      }
       return;
     }
 
@@ -440,6 +466,22 @@ public class Autoboxer extends ErrorReportingASTVisitor {
         args.set(i, replacementArg);
       }
     }
+  }
+
+  // Returns a list of booleans indicating whether the format specifier
+  // refers to an object argument or not.
+  private List<Boolean> getFormatSpecifiers(String format) {
+    List<Boolean> specifiers = Lists.newArrayList();
+    int i = 0;
+    while ((i = format.indexOf('%', i)) != -1) {
+      if (format.charAt(++i) == '%') {
+        // Skip %% sequences, since they don't have associated args.
+        ++i;
+        continue;
+      }
+      specifiers.add(format.charAt(i) == '@');
+    }
+    return specifiers;
   }
 
   private Expression boxOrUnboxExpression(Expression arg, ITypeBinding argType) {
