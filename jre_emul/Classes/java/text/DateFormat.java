833b34f0edef86d1589d57205a6b25057ab2ed1c
Tom Ball <tball@google.com>
2013-09-12 11:12:16 -0700

    Created iOS version of Android's ICU class, implementing its LocaleData support. 	Change on 2013/08/23 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=51316133

---
+++ jre_emul/Classes/java/text/DateFormat.java
@@ -0,0 +1,14 @@
+package java.text;
+
+/**
+ * Temporary class that defines DateFormat constants used by LocaleData.
+ *
+ * TODO(user): replace with full version.
+ */
+public class DateFormat extends Format {
+  
+  public static final int FULL = 0;
+  public static final int LONG = 1;
+  public static final int MEDIUM = 2;
+  public static final int SHORT = 3;
+}
