6752f0ab8d9931dabe5ca6eca40b4a730ef0d833
Tom Ball <tball@google.com>
2013-09-12 11:36:17 -0700

    Added remaining forward ref. 	Change on 2013/09/10 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=52120811

--- jre_emul/Classes/IOSReference.m
+++ jre_emul/Classes/IOSReference.m
@@ -69,6 +69,7 @@ static void EnsureReferentSubclass(id referent);
 static void RemoveReferenceAssociation(id referent, JavaLangRefReference *reference);
 static void RealReferentRelease(id referent);
 static void ReferentSubclassDealloc(id self, SEL _cmd);
+static void ReferentSubclassRelease(id self, SEL _cmd);
 static void WhileLocked(void (^block)(void));
 
 // Global recursive mutux.
