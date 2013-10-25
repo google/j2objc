e743669703b22413e6b3cec3922ed9ac24cf48d3
Tom Ball <tball@google.com>
2013-10-24 17:54:12 -0700

    Add deprecated attributes to enum types. 	Change on 2013/09/25 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=53276197

--- translator/src/main/java/com/google/devtools/j2objc/gen/ObjectiveCHeaderGenerator.java
+++ translator/src/main/java/com/google/devtools/j2objc/gen/ObjectiveCHeaderGenerator.java
@@ -119,10 +119,8 @@ public class ObjectiveCHeaderGenerator extends ObjectiveCSourceFileGenerator {
 
     printConstantDefines(node);
 
-    if (Options.generateDeprecatedDeclarations()) {
-      if (hasDeprecated(ASTUtil.getModifiers(node))) {
-        println(DEPRECATED_ATTRIBUTE);
-      }
+    if (needsDeprecatedAttribute(ASTUtil.getModifiers(node))) {
+      println(DEPRECATED_ATTRIBUTE);
     }
 
     if (isInterface) {
@@ -299,6 +297,10 @@ public class ObjectiveCHeaderGenerator extends ObjectiveCSourceFileGenerator {
       }
     }
 
+    if (needsDeprecatedAttribute(ASTUtil.getModifiers(node))) {
+      println(DEPRECATED_ATTRIBUTE);
+    }
+
     // Print enum type.
     printf("@interface %s : JavaLangEnum < NSCopying", typeName);
     ITypeBinding enumType = Types.getTypeBinding(node);
@@ -357,10 +359,8 @@ public class ObjectiveCHeaderGenerator extends ObjectiveCSourceFileGenerator {
          result += " OBJC_METHOD_FAMILY_NONE";
        }
 
-    if (Options.generateDeprecatedDeclarations()) {
-      if (hasDeprecated(ASTUtil.getModifiers(m))) {
-        result += " " + DEPRECATED_ATTRIBUTE;
-      }
+    if (needsDeprecatedAttribute(ASTUtil.getModifiers(m))) {
+      result += " " + DEPRECATED_ATTRIBUTE;
     }
 
     return result + ";\n";
@@ -648,6 +648,10 @@ public class ObjectiveCHeaderGenerator extends ObjectiveCSourceFileGenerator {
     if (hadConstant) {
       newline();
     }
+  }
+
+  private boolean needsDeprecatedAttribute(List<IExtendedModifier> modifiers) {
+    return Options.generateDeprecatedDeclarations() && hasDeprecated(modifiers);
   }
 
   /**
