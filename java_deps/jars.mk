233621da518f68b7f5e79cfc348a7efcf4fbf8f5
Tom Ball <tball@google.com>
2013-11-06 15:19:10 -0800

    Fixed public jsr305 build, updated to version 2.0.2.

--- java_deps/jars.mk
+++ java_deps/jars.mk
@@ -32,8 +32,8 @@ GUAVA_JAR = guava-13.0.jar
 ICU4J_JAR = icu4j-51.1.jar
 ICU4J_SOURCE_JAR = icu4j-51.1-sources.jar
 
-JSR305_JAR = jsr305-2.0.1.jar
-JSR305_SOURCE_JAR = jsr305-2.0.1.jar
+JSR305_JAR = jsr305-2.0.2.jar
+JSR305_SOURCE_JAR = jsr305-2.0.2.jar
 
 JUNIT_JAR = junit-4.10.jar
 JUNIT_SOURCE_JAR = junit-4.10-sources.jar
