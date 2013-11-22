92f863fd27acf745bacb56b877fc42ad75a8c533
Keith Stanger <kstanger@google.com>
2013-11-22 13:58:42 -0500

    Adds create_jar.mk and j2objc_deps.mk to the opensource filegroup. 	Change on 2013/11/22 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=57123559

---
+++ make/j2objc_deps.mk
@@ -0,0 +1,92 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Defines all of the top-level dependencies for the various j2objc components.
+#
+# Author: Keith Stanger
+
+# Call recursively only from the top-level makefile and not from xcode.
+ifeq ("$(CONFIGURATION_BUILD_DIR)x$(MAKELEVEL)", "x0")
+J2OBJC_CALL_RECURSIVE_DEPS = YES
+endif
+
+# Allow recursive calls from the j2objc root makefile.
+ifeq ($(CURDIR), $(realpath $(J2OBJC_ROOT)))
+J2OBJC_CALL_RECURSIVE_DEPS = YES
+endif
+
+ifeq ("$(J2OBJC_CALL_RECURSIVE_DEPS)", "YES")
+
+annotations_dist:
+	@$(MAKE) -C $(J2OBJC_ROOT)/annotations dist
+
+java_deps_dist:
+	@$(MAKE) -C $(J2OBJC_ROOT)/java_deps dist
+
+jre_emul_jar_dist: annotations_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/jre_emul emul_jar_dist
+
+translator: annotations_dist java_deps_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/translator dist
+
+translator_dist: translator jre_emul_jar_dist
+
+jre_emul_dist: translator_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/jre_emul dist
+
+jre_emul_java_manifest:
+	@$(MAKE) -C $(J2OBJC_ROOT)/jre_emul java_sources_manifest
+
+junit_dist: translator_dist jre_emul_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/junit dist
+
+junit_java: java_deps_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/junit java
+
+junit_manifest:
+	@$(MAKE) -C $(J2OBJC_ROOT)/junit java_sources_manifest
+
+jsr305_dist: translator_dist jre_emul_dist java_deps_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/jsr305 dist
+
+guava_dist: translator_dist jre_emul_dist jsr305_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/guava dist
+
+guava_java: java_deps_dist annotations_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/guava java
+
+guava_manifest:
+	@$(MAKE) -C $(J2OBJC_ROOT)/guava java_sources_manifest
+
+cycle_finder_dist: annotations_dist java_deps_dist translator_dist
+	@$(MAKE) -C $(J2OBJC_ROOT)/cycle_finder dist
+
+
+else
+
+annotations_dist:
+java_deps_dist:
+jre_emul_jar_dist:
+translator:
+translator_dist:
+jre_emul_dist:
+jre_emul_java_manifest:
+junit_dist:
+junit_java:
+junit_manifest:
+jsr305_dist:
+guava_dist:
+guava_java:
+guava_manifest:
+cycle_finder_dist:
+
+endif
