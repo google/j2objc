b7f0689265e49fc015b001e96b74fb8b56c8d587
Keith Stanger <kstanger@google.com>
2013-11-22 13:58:42 -0500

    Adds create_jar.mk and j2objc_deps.mk to the opensource filegroup. 	Change on 2013/11/22 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=57123559

---
+++ make/create_jar.mk
@@ -0,0 +1,36 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Provides rules for creating a jar file from a list of java sources.
+#
+# The including makefile may define the variables:
+#   CREATE_JAR_NAME
+#   CREATE_JAR_SOURCES
+#   CREATE_JAR_JAVAC_ARGS
+# The including makefile may use the following variables:
+#   CREATE_JAR_RESULT
+#
+# Author: Keith Stanger
+
+CREATE_JAR_RESULT = $(BUILD_DIR)/$(CREATE_JAR_NAME).jar
+CREATE_JAR_STAGE_DIR := $(shell echo /tmp/j2objc_$(CREATE_JAR_NAME)_$$$$)
+
+jar: $(CREATE_JAR_RESULT)
+	@:
+
+$(CREATE_JAR_RESULT): $(CREATE_JAR_SOURCES)
+	@mkdir -p $(@D)
+	@echo "Building $(notdir $@)"
+	@rm -rf $(CREATE_JAR_STAGE_DIR)
+	@mkdir $(CREATE_JAR_STAGE_DIR)
+	@$(JAVAC) $(CREATE_JAR_JAVAC_ARGS) -d $(CREATE_JAR_STAGE_DIR) $^
+	@jar cf $@ -C $(CREATE_JAR_STAGE_DIR) .
