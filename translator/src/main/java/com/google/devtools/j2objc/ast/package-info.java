9835f0083d093162fabc57341aab238ac1ec60ff
Tom Ball <tball@google.com>
2014-07-25 14:00:36 -0700

    Adds a package-info.java file to the ast package. 	Change on 2014/07/16 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=71288779

---
+++ translator/src/main/java/com/google/devtools/j2objc/ast/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * An abstract syntax tree used to represent the Java source being translated.
+ * For convenience the types mirror the structure of the Eclipse Java DOM/AST.
+ * @see <a href="http://help.eclipse.org/juno/topic/org.eclipse.jdt.doc.isv/reference/api/org/eclipse/jdt/core/dom/package-summary.html">
+ * org.eclipse.jdt.core.dom</a>
+ */
+
+package com.google.devtools.j2objc.ast;
