1fe2362b9e8411d3e1b14aa5ac873acefb42bd0b
Tom Ball <tball@google.com>
2014-08-27 10:45:47 -0700

    Added branch pruning when test is constant, like javac has. 	Change on 2014/08/20 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=73744953

---
+++ translator/src/test/java/com/google/devtools/j2objc/translate/ConstantBranchPrunerTest.java
@@ -0,0 +1,146 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.j2objc.translate;
+
+import com.google.devtools.j2objc.GenerationTest;
+
+import java.io.IOException;
+
+/**
+ * Unit tests for {@link ConstantBranchPruner}.
+ *
+ * @author Tom Ball
+ */
+public class ConstantBranchPrunerTest extends GenerationTest {
+
+  // Verify that statements with non-constant boolean expressions aren't modified.
+  public void testNoPruning() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { "
+        + "int test(boolean b) { if (b) { return 1; } else { return 0; }}"
+        + "String describe() { return \"is true? \" + true; }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "if (b) {", "return 1;", "}", "else {", "return 0;", "}");
+    assertTranslation(translation, "return @\"is true? true\";");
+    translation = translateSourceFile(
+        "class Test { void tick() {} void test(boolean b) { while (b) { tick(); } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "while (b) {", "[self tick];", "}");
+    translation = translateSourceFile(
+        "class Test { void tick() {} void test(boolean b) { do { tick(); } while (b); }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "do {", "[self tick];", "}", "while (b);");
+  }
+
+  // Verify then block replaces if statement when true.
+  public void testTrueIfExpression() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { int test() { if (true) { return 1; } else { return 0; } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "- (int)test {", "{", "return 1;", "}", "}");
+    translation = translateSourceFile(
+        "class Test { static final boolean debug = true;"
+        + "  int test() { if (debug) { return 1; } else { return 0; } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "- (int)test {", "{", "return 1;", "}", "}");
+  }
+
+  // Verify else block replaces if statement when false.
+  public void testFalseIfExpression() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { int test() { if (false) { return 1; } else { return 0; } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "- (int)test {", "{", "return 0;", "}", "}");
+    translation = translateSourceFile(
+        "class Test { static final boolean debug = false;"
+        + "  int test() { if (debug) { return 1; } else { return 0; } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "- (int)test {", "{", "return 0;", "}", "}");
+  }
+
+  // Verify parentheses surround boolean constants are removed.
+  public void testParentheses() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { int test() { if (((false))) { return 1; } else { return 0; } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "- (int)test {", "{", "return 0;", "}", "}");
+  }
+
+  // Verify ! boolean constant is inverted.
+  public void testBooleanInversion() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { static final boolean debug = true;"
+        + "  int test() { if (!(debug)) { return 1; } else { return 0; } }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation, "- (int)test {", "{", "return 0;", "}", "}");
+  }
+
+  // Verify && expressions are pruned correctly.
+  public void testAnd() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { "
+        + "static final boolean DEBUG = true; "
+        + "static final boolean NDEBUG = false;"
+        + "int test() { int result; "
+        + "  if (DEBUG && true) { result = 1; } else { result = 2; }"
+        + "  if (DEBUG && false) { result = 3; } else { result = 4; }"
+        + "  if (true && NDEBUG) { result = 5; } else { result = 6; }"
+        + "  if (false && NDEBUG) { result = 7; } else { result = 8; }"
+        + "  return result; }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation,
+        "{", "result = 1;", "}",
+        "{", "result = 4;", "}",
+        "{", "result = 6;", "}",
+        "{", "result = 8;", "}");
+  }
+
+  // Verify || expressions are pruned correctly.
+  public void testOr() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { "
+        + "static final boolean DEBUG = true; "
+        + "static final boolean NDEBUG = false;"
+        + "int test() { int result; "
+        + "  if (DEBUG || true) { result = 1; } else { result = 2; }"
+        + "  if (DEBUG || false) { result = 3; } else { result = 4; }"
+        + "  if (true || NDEBUG) { result = 5; } else { result = 6; }"
+        + "  if (false || NDEBUG) { result = 7; } else { result = 8; }"
+        + "  return result; }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation,
+        "{", "result = 1;", "}",
+        "{", "result = 3;", "}",
+        "{", "result = 5;", "}",
+        "{", "result = 8;", "}");
+  }
+
+  // Verify method invocation paired with constant is pruned.
+  public void testMethodAnd() throws IOException {
+    String translation = translateSourceFile(
+        "class Test { "
+        + "static final boolean DEBUG = true; "
+        + "static final boolean NDEBUG = false;"
+        + "boolean enabled() { return true; }"
+        + "int test() { int result; "
+        + "  if (enabled() && NDEBUG) { result = 1; } else { result = 2; }"
+        + "  if (enabled() || DEBUG) { result = 3; } else { result = 4; }"
+        + "  return result; }}",
+        "Test", "Test.m");
+    assertTranslatedLines(translation,
+        "{", "result = 2;", "}",
+        "{", "result = 3;", "}");
+  }
+}
