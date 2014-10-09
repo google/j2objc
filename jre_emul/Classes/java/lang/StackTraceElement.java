7d145bbf55cc9eb219b6923adaa19a6308bb5d1a
kstanger <kstanger@google.com>
2014-10-09 11:36:26 -0700

    Improve parsing of stack symbols for new functionized methods. 	Change on 2014/10/09 by kstanger <kstanger@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=77362749

--- jre_emul/Classes/java/lang/StackTraceElement.java
+++ jre_emul/Classes/java/lang/StackTraceElement.java
@@ -110,31 +110,32 @@ public class StackTraceElement {
     void *shortStack[1];
     shortStack[0] = (void *)self->address_;
     char **stackSymbol = backtrace_symbols(shortStack, 1);
+    NSStringEncoding encoding = [NSString defaultCStringEncoding];
 
     // Extract hexAddress.
     char *start = strstr(*stackSymbol, "0x");  // Skip text before address.
     char *addressEnd = strstr(start, " ");
     char *hex = strndup(start, addressEnd - start);
-    self->hexAddress_ = [[NSString alloc] initWithCString:hex
-                                                 encoding:[NSString defaultCStringEncoding]];
+    self->hexAddress_ = [[NSString alloc] initWithCString:hex encoding:encoding];
     free(hex);
     start = addressEnd + 1;
+
+    // Extract the offset if symbol looks like "<method> + 123".
+    char *offset = strstr(start, " + ");
+    if (offset) {
+      self->offset_ = [[NSString alloc] initWithCString:offset + 3 encoding:encoding];
+      *offset = '\0';
+    }
 
     // See if a class and method names can be extracted.
     char *leftBrace = strchr(start, '[');
     char *rightBrace = strchr(start, ']');
-    if (rightBrace && strlen(rightBrace) > 4) {  // If pattern is similar to: ...] + 123
-      // Save trailing function address offset, then "remove" it.
-      self->offset_ = [[NSString alloc] initWithCString:rightBrace + 4
-                                               encoding:[NSString defaultCStringEncoding]];
-      *(rightBrace + 1) = '\0';
-    }
     if (leftBrace && rightBrace && (rightBrace - leftBrace) > 0) {
       char *signature = leftBrace + 1;
       char *className = strsep(&signature, "[ ]");
       if (className && strlen(className) > 0) {
-        IOSClass *cls = [IOSClass classForIosName:[NSString stringWithCString:className
-            encoding:[NSString defaultCStringEncoding]]];
+        IOSClass *cls = [IOSClass classForIosName:
+            [NSString stringWithCString:className encoding:encoding]];
         if (cls) {
           self->className__ = RETAIN_([cls getName]);
         }
@@ -164,14 +165,39 @@ public class StackTraceElement {
           methodName = selector;
         }
         if (methodName) {
-          self->methodName_ = [[NSString alloc] initWithCString:methodName
-                                                       encoding:[NSString defaultCStringEncoding]];
+          self->methodName_ = [[NSString alloc] initWithCString:methodName encoding:encoding];
         }
       }
     } else {
+      // Functionized method. Look for the class name portion.
+      IOSClass *cls = nil;
+      // Search backwards for '_' so that we find inner classes before their
+      // outer class.
+      char *idx = start;
+      while (*idx) {
+        idx++;
+      }
+      while (!cls) {
+        while (--idx > start && *idx != '_');
+        if (idx == start) {
+          break;
+        }
+        NSString *className = [[[NSString alloc] initWithBytesNoCopy:start
+                                                              length:idx - start
+                                                            encoding:encoding
+                                                        freeWhenDone:NO] autorelease];
+        cls = [IOSClass classForIosName:className];
+      }
+      if (cls) {
+        self->className__ = RETAIN_([cls getName]);
+        start = idx + 1;
+      }
+      char *paramsStart = strstr(start, "With");
+      if (paramsStart) {
+        *paramsStart = '\0';
+      }
       // Copy rest of stack symbol to methodName.
-      self->methodName_ = [[NSString alloc] initWithCString:start
-                                                   encoding:[NSString defaultCStringEncoding]];
+      self->methodName_ = [[NSString alloc] initWithCString:start encoding:encoding];
     }
     free(stackSymbol);
   ]-*/;
