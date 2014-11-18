7059edaf183760fd81727fd4dde58e44c82f1376
tball <tball@google.com>
2014-11-18 12:34:05 -0800

    Create builddefs/BUILD during running make dist. 	Change on 2014/11/18 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=80227239

---
+++ annotations/classes.mk
@@ -0,0 +1,20 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ANNOTATION_SOURCE_JAVA = \
+    com/google/j2objc/annotations/AutoreleasePool.java \
+    com/google/j2objc/annotations/LoopTranslation.java \
+    com/google/j2objc/annotations/ObjectiveCName.java \
+    com/google/j2objc/annotations/ReflectionSupport.java \
+    com/google/j2objc/annotations/RetainedLocalRef.java \
+    com/google/j2objc/annotations/Weak.java \
+    com/google/j2objc/annotations/WeakOuter.java
