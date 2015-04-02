3431b676caa8da5e019f46e82be89631fc154602
mthvedt <mthvedt@google.com>
2015-04-02 10:03:46 -0700

    Fix test broken by [] 	Change on 2015/04/02 by mthvedt <mthvedt@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=90170738

--- translator/src/test/java/com/google/devtools/j2objc/gen/LineDirectivesTest.java
+++ translator/src/test/java/com/google/devtools/j2objc/gen/LineDirectivesTest.java
@@ -81,7 +81,10 @@ public class LineDirectivesTest extends GenerationTest {
         + "  }}\n",
         "A", "A.m");
     assertTranslation(translation, "#line 1 \"A.java\"");
-    assertTranslation(translation, "#line 1\n@implementation A");
+    assertTranslatedLines(translation,
+        "#line 0 \"" + tempDir + "/A.java\"",
+        "",
+        "@implementation A");
     assertTranslation(translation, "#line 2\n- (NSString *)test");
     assertTranslation(translation, "#line 4\n  jint i = 0;");
     assertTranslation(translation, "#line 7\n  return JavaLangInteger_toStringWithInt_(i);");
