f6dd5bdb180a33550ba2b8a58a5784f23b7c83d5
Tom Ball <tball@google.com>
2015-05-26 11:25:18 -0700

    Fixed sidebar navigation.

--- _includes/nav.js
+++ _includes/nav.js
@@ -1,24 +1,51 @@
-document.documentElement.classList.add('js');
+function saveCollapseState() {
+  var menus = document.querySelectorAll('nav#menu .submenu');
+  var expandedMenus = [];
+  Array.prototype.forEach.call(menus, function (menu, index) {
+    if (menu.className.indexOf('collapsed') == -1) {
+      expandedMenus.push(index.toString());
+    }
+  });
+  document.cookie = "nav_state=" + expandedMenus.join(',');
+}
+
+function getCollapseState() {
+  var cname = "nav_state=";
+  var cookies = document.cookie.split(';');
+  for (var i = 0; i < cookies.length; i++) {
+    var c = cookies[i].trim();
+    if (c.indexOf(cname) == 0) {
+      return c.substring(cname.length, c.length).split(',');
+    }
+  }
+  return [];
+}
 
 document.onreadystatechange = function () {
   switch (document.readyState) {
     case 'interactive':
-      var groups = document.querySelectorAll('nav#menu .group');
-      Array.prototype.forEach.call(groups, function (group) {
-        var anchor = group.querySelector('[name=name]');
-        var list = group.querySelector('[name=list]');
-        var active = list.querySelector('.active');
+      var menus = document.querySelectorAll('nav#menu .submenu');
+      var nav_state = getCollapseState();
+      Array.prototype.forEach.call(menus, function (menu, index) {
+        var anchor = menu.querySelector('[name=subheader]');
+        var list = menu.querySelector('[name=list]');
+        if (nav_state.indexOf(index.toString()) > -1) {
+          menu.classList.toggle('collapsed', false);
+          var active = true;
+        } else {
+          var active = list.querySelector('.active');
+          menu.classList.toggle('collapsed', ! active);
+        }
         list.dataset.height = list.clientHeight + 'px';
         list.style.maxHeight = active ? list.dataset.height : '0px';
-        group.classList.toggle('collapsed', ! active);
         anchor.addEventListener('click', function() {
           list.style.maxHeight = (list.style.maxHeight == '0px') ? list.dataset.height : '0px';
-          group.classList.toggle('collapsed');
+          menu.classList.toggle('collapsed');
+          saveCollapseState();
         });
       });
       break;
     case 'complete':
-      // When the doc is fully loaded, turn on animation
       document.querySelector('nav#menu').classList.add('animate');
       break;
   }
