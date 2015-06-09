5a9a1e8f1fa77356b91c01d73a4d076f4ce58b12
Seth Kirby <mr.seth.kirby@gmail.com>
2015-06-09 12:51:03 -0700

    WIP changing to material design with new template.

---
+++ sidebar_gen/sidebar.py
@@ -0,0 +1,46 @@
+def processLines(sidebarLines,firstFlag = False):
+	if len(sidebarLines) == 0:
+		print '</ul>'
+		print '</li>'
+	elif sidebarLines[0][0] != " ":
+		if (not firstFlag):
+			print '</ul>'
+			print '</li>'
+		print '<li class="devsite-nav-item devsite-nav-item-section-expandable">'
+		print '<a class="devsite-nav-title devsite-nav-title-no-path " tabindex="0">'
+		print '<span>{}</span></a>'.format(sidebarLines[0])
+		print '<a class="devsite-nav-toggle devsite-nav-toggle-expandable"></a>'
+		print '<ul class="devsite-nav-section devsite-nav-section-expandable">'
+		processLines(sidebarLines[1:])
+	else:
+		link = sidebarLines[0].split()[0]
+		text = ' '.join(sidebarLines[0].split()[1:])
+		print '<li class="devsite-nav-item"><a href="{}" class="devsite-nav-title gc-analytics-event " data-category="Site-Wide Custom Events" data-label="Hamburger menu" data-action="Link Click">'.format(link)
+		print '<span>{}</span></a></li>'.format(text)
+		processLines(sidebarLines[1:])
+
+def buildSidebar(sidebarLines):
+	print '<nav class="devsite-section-nav-responsive devsite-nav" tabindex="0" style="left: -256px;">'
+	print '<ul class="devsite-nav-expandable">'
+	processLines(sidebarLines,True)
+	print '</ul>'
+	print '</nav>'
+	print '<div class="devsite-main-content clearfix" style="margin-top: 40px;">'
+	print '<nav class="devsite-section-nav devsite-nav" style="left: auto; max-height: 737px; position: relative; top: 0px;">'
+	print '<ul class="devsite-nav-expandable">'
+	processLines(sidebarLines,True)
+	print '</ul>'
+	print '</nav>'
+	print '<nav class="devsite-page-nav devsite-nav" style="position: relative; left: auto; max-height: 737px; top: 0px;"></nav>'
+
+
+
+def removeBlankLines(ls):
+	return filter(lambda x: x, ls)
+
+if __name__ == "__main__":
+	import sys
+	sys.stdout = open('sidebar.html', 'w')
+	sidebarLines = open("sidebar.txt").readlines()
+	sidebarLines = removeBlankLines(sidebarLines)
+	buildSidebar(sidebarLines)
