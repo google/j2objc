ff5dc3c7bad75cbaf073b30ef0a1ed980c555eb1
mthvedt <mthvedt@google.com>
2015-06-22 08:57:24 -0700

    Support for use of standard JUnit in J2ObjC. 	Change on 2015/06/22 by mthvedt <mthvedt@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=96572630

---
+++ testing/junit-ext/src/objc/TestRunnerMain.m
@@ -0,0 +1,5 @@
+#import "third_party/java_src/j2objc/testing/junit-ext/src/java/com/google/j2objc/testing/JUnitTestRunner.h"
+
+int main(int argc, char *argv[]) {
+  return [ComGoogleJ2objcTestingJUnitTestRunner mainWithNSStringArray:nil];
+}
