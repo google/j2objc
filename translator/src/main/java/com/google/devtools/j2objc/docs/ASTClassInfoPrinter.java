523f299a9c832f67787a5f3add4e001dd8c515d9
tball <tball@google.com>
2015-06-23 12:08:58 -0700

    Cleaned up class-info documentation printer. 	Change on 2015/06/23 by tball <tball@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=96696315

--- translator/src/main/java/com/google/devtools/j2objc/docs/ASTClassInfoPrinter.java
+++ translator/src/main/java/com/google/devtools/j2objc/docs/ASTClassInfoPrinter.java
@@ -1,4 +1,18 @@
-package com.google.devtools.j2objc.tools;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.j2objc.docs;
 
 import com.google.common.collect.Sets;
 import com.google.common.collect.TreeMultimap;
@@ -11,11 +25,13 @@ import junit.framework.TestCase;
 import java.io.IOException;
 import java.util.Set;
 
-
 /**
- * Helper functions for printing AST tree.
+ * Prints a markdown compliant list of our AST nodes by ancestry, with Objects not in the Eclipse
+ * AST bolded. Provides a quick visual of inheritance and J2ObjC AST nodes for project docs.
+ *
+ * @author Seth Kirby
  */
-public class Info {
+public class ASTClassInfoPrinter {
   private static Set<String> astLookup;
   private static TreeMultimap<String, String> tree;
 
@@ -30,24 +46,16 @@ public class Info {
 
   private static void printClassHierarchy(String node, String indent) {
     if (astLookup.contains(node)) {
-      println(indent + "- " + node);
+      System.out.println(indent + "- " + node);
     } else {
-      println(indent + "- **" + node + "**");
+      System.out.println(indent + "- **" + node + "**");
     }
     for (String childNode : tree.get(node)) {
       printClassHierarchy(childNode, "  " + indent);
     }
   }
 
-  public static void println(Object x) {
-    System.out.println(x);
-  }
-
-  /**
-   * Prints a markdown compliant list of our AST nodes by ancestry, with Objects not in the Eclipse
-   * AST bolded. Provides a quick visual of inheritance and J2ObjC AST nodes for output in the wiki.
-   */
-  public static void printASTClassInfo() {
+  public static void main(String... args) {
     // Clear saved state for new calls.
     tree = TreeMultimap.create();
     astLookup = Sets.newHashSet();
