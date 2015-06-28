bbbf5be03b3e367602cd24dc29c2743e59ef68c9
Lukhnos Liu <lukhnos@lukhnos.org>
2015-06-27 20:17:24 -0700

    Fix pre/postincrement of parenthesized array access.

--- translator/src/test/java/com/google/devtools/j2objc/translate/ArrayRewriterTest.java
+++ translator/src/test/java/com/google/devtools/j2objc/translate/ArrayRewriterTest.java
@@ -17,8 +17,10 @@
 package com.google.devtools.j2objc.translate;
 
 import com.google.devtools.j2objc.GenerationTest;
+import com.google.devtools.j2objc.ast.Statement;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Unit tests for {@link ArrayRewriter} class.
@@ -39,5 +41,17 @@ public class ArrayRewriterTest extends GenerationTest {
         "IOSObjectArray_SetAndConsume(nil_chk(o), 0, new_NSObject_init());");
     assertTranslation(translation,
         "IOSObjectArray_SetAndConsume(o, 0, [IOSIntArray newArrayWithLength:1]);");
+  }
+
+  public void testPreAndPostincrementOfArrayAccess() throws IOException {
+    String source = "int x[] = { 0 }; ++x[0]; x[0]++; ++(x[0]); (x[0])++; ++((x[0])); ((x[0]))++;";
+    List<Statement> stmts = translateStatements(source);
+    assertEquals(7, stmts.size());
+    assertEquals("++(*IOSIntArray_GetRef(x, 0));", generateStatement(stmts.get(1)));
+    assertEquals("(*IOSIntArray_GetRef(x, 0))++;", generateStatement(stmts.get(2)));
+    assertEquals("++(*IOSIntArray_GetRef(x, 0));", generateStatement(stmts.get(3)));
+    assertEquals("(*IOSIntArray_GetRef(x, 0))++;", generateStatement(stmts.get(4)));
+    assertEquals("++((*IOSIntArray_GetRef(x, 0)));", generateStatement(stmts.get(5)));
+    assertEquals("((*IOSIntArray_GetRef(x, 0)))++;", generateStatement(stmts.get(6)));
   }
 }
