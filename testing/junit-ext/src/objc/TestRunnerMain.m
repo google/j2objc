17ada4d5a3f34b5fa33fa74827f95ccf88d1b704
pdonelan <pdonelan@google.com>
2015-11-09 17:21:12 -0800

    Ensure that segfaults in j2objc's JUnit test runner are reported as failures
    Detect SIGSEGV and print output so that mobileharness detects test failure
    (see: mobileharness/shared/ios/IosUtil.java).
    	Change on 2015/11/09 by pdonelan <pdonelan@google.com>
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=107439906

--- testing/junit-ext/src/objc/TestRunnerMain.m
+++ testing/junit-ext/src/objc/TestRunnerMain.m
@@ -1,5 +1,36 @@
 #import "third_party/java_src/j2objc/testing/junit-ext/src/java/com/google/j2objc/testing/JUnitTestRunner.h"
 
+#include <errno.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+
+static void signalHandler(int sig) {
+  // Ensure this looks like a GTM_UNIT_TESTING failure.
+  printf("[  FAILED  ]\n");
+
+  // Get void*'s for all entries on the stack.
+  void *array[64];
+  int frame_count = (int) backtrace(array, 64);
+
+  // Print all the frames to stderr.
+  fprintf(stderr, "Terminating j2objc JUnit TestRunnerMain.m due to signal: %d\n", sig);
+  fprintf(stderr, "Stack trace:\n");
+  backtrace_symbols_fd(array, frame_count, 2);
+
+  exit(EXIT_FAILURE);
+}
+
+void installSignalHandler() {
+  signal(SIGABRT, signalHandler);
+  signal(SIGILL, signalHandler);
+  signal(SIGSEGV, signalHandler);
+  signal(SIGFPE, signalHandler);
+  signal(SIGBUS, signalHandler);
+  signal(SIGPIPE, signalHandler);
+}
+
 int main(int argc, char *argv[]) {
+  installSignalHandler();
   return [ComGoogleJ2objcTestingJUnitTestRunner mainWithNSStringArray:nil];
 }
