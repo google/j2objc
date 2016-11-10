77924fd90767309b15ae9946716b4ae607a9fedd
tball <tball@google.com>
2016-11-10 09:40:43 -0800

    Add -Wduplicate-method-match warning, to catch duplicate method declarations.
    	Change on 2016/11/10 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=138770243

--- make/common.mk
+++ make/common.mk
@@ -107,7 +107,8 @@ endif
 DEBUGFLAGS := $(DEBUGFLAGS) -O$(OPTIMIZATION_LEVEL)
 
 CC_WARNINGS = -Wall -Werror -Wshorten-64-to-32 -Wimplicit-function-declaration \
-  -Wmissing-field-initializers -Wno-unused-variable -Wno-nullability-completeness
+  -Wmissing-field-initializers -Wduplicate-method-match -Wno-unused-variable \
+  -Wno-nullability-completeness
 
 ifdef GCC_PREPROCESSOR_DEFINITIONS
 DEBUGFLAGS += $(GCC_PREPROCESSOR_DEFINITIONS:%=-D%)
