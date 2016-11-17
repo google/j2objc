d07a94bd0a63897e2387738184c5425fc7852acd
kstanger <kstanger@google.com>
2016-11-17 09:50:26 -0800

    Fixes race conditions for initializing @RetainedWith fields in ConcurrentHashMap. Previously the accessors would allow race conditions that caused multiple initializations if the view fields, but that behavior is invalid when @RetainedWith is used on the fields.
    	Change on 2016/11/17 by kstanger <kstanger@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=139472144

--- jre_emul/android/libcore/luni/src/main/java/java/util/concurrent/ConcurrentHashMap.java
+++ jre_emul/android/libcore/luni/src/main/java/java/util/concurrent/ConcurrentHashMap.java
@@ -648,6 +648,10 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K, V>
     private transient volatile CounterCell[] counterCells;
 
     // views
+    // J2ObjC -- Added @RetainedWith annotations to resolve cycles between these views and the
+    // containing map. The accessors must be synchronized because with @RetainedWith we can no
+    // longer allow the race condition where the view field is assigned twice if the same accessor
+    // is called concurrently by multiple threads.
     @RetainedWith private transient KeySetView<K,V> keySet;
     @RetainedWith private transient ValuesView<K,V> values;
     @RetainedWith private transient EntrySetView<K,V> entrySet;
@@ -1065,7 +1069,7 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K, V>
      * @return the set view
      */
     // android-note : changed KeySetView<K,V> to Set<K> to maintain API compatibility.
-    public Set<K> keySet() {
+    public synchronized Set<K> keySet() {
         KeySetView<K,V> ks;
         return (ks = keySet) != null ? ks : (keySet = new KeySetView<K,V>(this, null));
     }
@@ -1088,7 +1092,7 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K, V>
      *
      * @return the collection view
      */
-    public Collection<V> values() {
+    public synchronized Collection<V> values() {
         ValuesView<K,V> vs;
         return (vs = values) != null ? vs : (values = new ValuesView<K,V>(this));
     }
@@ -1110,7 +1114,7 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K, V>
      *
      * @return the set view
      */
-    public Set<Map.Entry<K,V>> entrySet() {
+    public synchronized Set<Map.Entry<K,V>> entrySet() {
         EntrySetView<K,V> es;
         return (es = entrySet) != null ? es : (entrySet = new EntrySetView<K,V>(this));
     }
