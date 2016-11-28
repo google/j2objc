17294c3b3ed7e20b7b8470428d9391a20ab8c705
tball <tball@google.com>
2016-11-28 10:25:40 -0800

    Fixed imports.
    	Change on 2016/11/28 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=140372932

--- translator/src/main/java/com/google/devtools/j2objc/util/ElementUtil.java
+++ translator/src/main/java/com/google/devtools/j2objc/util/ElementUtil.java
@@ -46,6 +46,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
+import javax.lang.model.element.Name;
 import javax.lang.model.element.NestingKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
