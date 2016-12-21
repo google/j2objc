2452416cdb6b371226b1e8cccadcb77847d74cdc
tball <tball@google.com>
2016-12-21 09:37:21 -0800

    Fixed missing positions in converted nodes.
    	Change on 2016/12/21 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=142671876

--- translator/src/main/java/com/google/devtools/j2objc/javac/TreeConverter.java
+++ translator/src/main/java/com/google/devtools/j2objc/javac/TreeConverter.java
@@ -646,14 +646,15 @@ public class TreeConverter {
 
   private TreeNode convertEnhancedForStatement(JCTree.JCEnhancedForLoop node) {
     return new EnhancedForStatement()
-        .setParameter((SingleVariableDeclaration) convertSingleVariable(node.getVariable()))
+        .setParameter((SingleVariableDeclaration) convertSingleVariable(node.getVariable())
+            .setPosition(getPosition(node)))
         .setExpression((Expression) convert(node.getExpression()))
         .setBody((Statement) convert(node.getStatement()));
   }
 
   private TreeNode convertEnum(JCTree.JCClassDecl node) {
     if (node.sym.isAnonymous()) {
-      return (TypeDeclaration) convertClassDeclaration(node);
+      return convertClassDeclaration(node).setPosition(getPosition(node));
     }
     EnumDeclaration newNode = (EnumDeclaration) new EnumDeclaration();
     convertBodyDeclaration(node, node.getModifiers(), newNode, node.sym);
@@ -728,7 +729,7 @@ public class TreeConverter {
     }
     if (node.getIdentifier().toString().equals("class")) {
       return new TypeLiteral(node.type)
-          .setType((Type) convertType(selected.type, pos, false));
+          .setType((Type) convertType(selected.type, pos, false).setPosition(getPosition(node)));
     }
     if (selected.getKind() == Kind.IDENTIFIER && !node.sym.getKind().isField()) {
       JCIdent ident = (JCTree.JCIdent) selected;
@@ -1029,7 +1030,7 @@ public class TreeConverter {
     PackageDeclaration newNode = new PackageDeclaration()
         .setPackageElement(pkg);
     for (JCTree.JCAnnotation pkgAnnotation : unit.getPackageAnnotations()) {
-      newNode.addAnnotation((Annotation) convertAnnotation(pkgAnnotation));
+      newNode.addAnnotation((Annotation) convert(pkgAnnotation));
     }
     return (PackageDeclaration) newNode.setName(convertName((PackageSymbol) pkg, getPosition(node)))
         .setJavadoc((Javadoc) getAssociatedJavaDoc(unit, pkg))
@@ -1081,8 +1082,9 @@ public class TreeConverter {
 
   private TreeNode convertSynchronized(JCTree.JCSynchronized node) {
     return new SynchronizedStatement()
-        .setExpression((Expression) convertWithoutParens(node.getExpression()))
-        .setBody((Block) convertBlock(node.getBlock()));
+        .setExpression((Expression) convertWithoutParens(node.getExpression())
+            .setPosition(getPosition(node)))
+        .setBody((Block) convert(node.getBlock()));
   }
 
   private TreeNode convertThrow(JCTree.JCThrow node) {
