0a7b610af7be422cd2a07a0d839597ee839ad626
tball <tball@google.com>
2017-01-08 12:24:05 -0800

    Removed debug message.
    	Change on 2017/02/21 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=148128778

--- jre_emul/Classes/com/google/j2objc/security/cert/IosX509Certificate.java
+++ jre_emul/Classes/com/google/j2objc/security/cert/IosX509Certificate.java
@@ -122,7 +122,6 @@ public class IosX509Certificate extends X509Certificate {
     // Verify it is valid for the specified date.
     double requestedTime = [date getTime] / 1000.0;
     NSDate *nsDate = [NSDate dateWithTimeIntervalSince1970:requestedTime];
-    NSLog(@"trust date: %@", nsDate);
     SecTrustResultType trustResult;
     SecTrustSetVerifyDate(trust, (ARCBRIDGE CFDateRef) nsDate);
     status = SecTrustEvaluate(trust, &trustResult);
