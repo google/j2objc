954e46ce61c64eb3aebee5f0e206a432f76c077a
kstanger <kstanger@google.com>
2017-04-12 11:03:48 -0700

    Check in local copies of mockito jars to fix our make build.
    	Change on 2017/04/12 by kstanger <kstanger@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=152960667

--- java_deps/jars.mk
+++ java_deps/jars.mk
@@ -17,7 +17,21 @@
 JARJAR_HOME = jarjar
 JARJAR_JAR = jarjar-1.4.jar
 JAVA_DEPS_JAR_DIR = $(J2OBJC_ROOT)/java_deps/$(BUILD_DIR_NAME)
+
 JUNIT_JAR = j2objc_junit.jar
+
+JUNIT_ROOT_JAR = junit-4.11.jar
+JUNIT_SOURCE_JAR = junit-4.11-sources.jar
+
+# JUnit dependency
+HAMCREST_JAR = hamcrest-core-1.3.jar
+HAMCREST_SOURCE_JAR = hamcrest-core-1.3-sources.jar
+
+MOCKITO_JAR = mockito-core-1.9.5.jar
+MOCKITO_SOURCE_JAR = mockito-core-1.9.5-sources.jar
+
+ASM_JAR = asm-5.0.4.jar
+ASM_SOURCE_JAR = asm-5.0.4-sources.jar
 
 PROTOBUF_JAR = protobuf-java-3.2.0.jar
 
@@ -42,17 +56,7 @@ JSR305_SOURCE_JAR = jsr305-3.0.0.jar
 JAVAX_INJECT_JAR = javax.inject-1.jar
 JAVAX_INJECT_SOURCE_JAR = javax.inject-1-sources.jar
 
-JUNIT_ROOT_JAR = junit-4.11.jar
-JUNIT_SOURCE_JAR = junit-4.11-sources.jar
-HAMCREST_JAR = hamcrest-core-1.3.jar
-HAMCREST_SOURCE_JAR = hamcrest-core-1.3-sources.jar
-
-MOCKITO_JAR = mockito-core-1.9.5.jar
-MOCKITO_SOURCE_JAR = mockito-core-1.9.5-sources.jar
-
 JAVAC_JAR = tools.jar
 
 DOCLAVA_JAR = doclava-1.0.6.jar
 JSILVER_JAR = jsilver-1.0.0.jar
-ASM_JAR = asm-5.0.4.jar
-ASM_SOURCE_JAR = asm-5.0.4-sources.jar
