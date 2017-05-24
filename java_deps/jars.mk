7412d392032e93e902ffaccfb09cd228a53e3827
kstanger <kstanger@google.com>
2017-05-24 11:45:07 -0700

    Updates the protocol buffers dependency to 3.3.0.
    	Change on 2017/05/24 by kstanger <kstanger@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=157016610

--- java_deps/jars.mk
+++ java_deps/jars.mk
@@ -33,7 +33,7 @@ MOCKITO_SOURCE_JAR = mockito-core-1.9.5-sources.jar
 ASM_JAR = asm-5.0.4.jar
 ASM_SOURCE_JAR = asm-5.0.4-sources.jar
 
-PROTOBUF_JAR = protobuf-java-3.2.0.jar
+PROTOBUF_JAR = protobuf-java-3.3.0.jar
 
 ECLIPSE_JARS = \
     org.eclipse.core.contenttype-3.4.200.v20140207-1251.jar \
