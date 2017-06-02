70f65d371993d32b2ce7f05af6dee0d05200747b
tball <tball@google.com>
2017-06-02 13:04:51 -0700

    Enforce anonymous class metadata has final modifier, per JLS 15.9.5.
    	Change on 2017/06/02 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=157865328

--- translator/src/main/java/com/google/devtools/j2objc/translate/MetadataWriter.java
+++ translator/src/main/java/com/google/devtools/j2objc/translate/MetadataWriter.java
@@ -467,6 +467,8 @@ public class MetadataWriter extends UnitTreeVisitor {
     if (ElementUtil.isAnonymous(type)) {
       // Anonymous classes are always static, though their closure may include an instance.
       modifiers |= ElementUtil.ACC_ANONYMOUS | java.lang.reflect.Modifier.STATIC;
+      // Anonymous classes are always implicitly final (JLS 15.9.5.).
+      modifiers |= java.lang.reflect.Modifier.FINAL;
     }
     return modifiers;
   }
