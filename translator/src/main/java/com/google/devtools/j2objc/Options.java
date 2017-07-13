23b1881b0e4ae08ce0f5847250e022e989657aa7
kstanger <kstanger@google.com>
2017-07-13 09:56:42 -0700

    Re-enable the disallowed inherited constructors.
    	Change on 2017/07/13 by kstanger <kstanger@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=161822365

--- translator/src/main/java/com/google/devtools/j2objc/Options.java
+++ translator/src/main/java/com/google/devtools/j2objc/Options.java
@@ -73,7 +73,7 @@ public class Options {
   private boolean staticAccessorMethods = false;
   private int batchTranslateMaximum = -1;
   private String processors = null;
-  private boolean disallowInheritedConstructors = false;
+  private boolean disallowInheritedConstructors = true;
   private boolean swiftFriendly = false;
   private boolean nullability = false;
   private EnumSet<LintOption> lintOptions = EnumSet.noneOf(LintOption.class);
