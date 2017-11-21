e5d4c836d040d34b5fa02760af44619ecd667c5c
tball <tball@google.com>
2017-11-21 10:12:03 -0800

    Added Documented annotation to j2objc class annotations.
    	Change on 2017/11/21 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=176525127

--- annotations/src/main/java/com/google/j2objc/annotations/ObjectiveCName.java
+++ annotations/src/main/java/com/google/j2objc/annotations/ObjectiveCName.java
@@ -14,6 +14,7 @@
 
 package com.google.j2objc.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -36,6 +37,7 @@ import java.lang.annotation.Target;
  *
  * @author Tom Ball
  */
+@Documented
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.PACKAGE })
 @Retention(RetentionPolicy.CLASS)
 public @interface ObjectiveCName {
