2897475d1f4784d2ff28d1769d7418de7ad35871
tball <tball@google.com>
2017-12-08 20:28:56 -0800

    Merge pull request #922: fix mock so it's instanceof original.
    	Change on 2017/12/08 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=178460261

--- testing/mockito/tests/MockMakerTest.java
+++ testing/mockito/tests/MockMakerTest.java
@@ -1,6 +1,6 @@
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -13,10 +13,10 @@ public class MockMakerTest {
     public void isAssinableFromMockObject() {
 
         ClassA mockClassA = Mockito.mock(ClassA.class);
-        
-        Assert.assertTrue(ClassA.class.isAssignableFrom(mockClassA.getClass()));
-    }   
-    
+
+        assertTrue(ClassA.class.isAssignableFrom(mockClassA.getClass()));
+    }
+
     @Test
     public void whenThenReturnMockObject() {
 
@@ -24,6 +24,22 @@ public class MockMakerTest {
         ClassA mockClassA = Mockito.mock(ClassA.class);
 
         when(mockInterfaceA.getClassA()).thenReturn(mockClassA);
+    }
+
+    @Test
+    public void shouldInstanceOfOriginalClass_true() {
+
+        ClassA mockClassA = Mockito.mock(ClassA.class);
+
+        assertTrue(mockClassA instanceof ClassA);
+    }
+
+    @Test
+    public void shouldInstanceOfOriginalInterface_true() {
+
+        InterfaceA mockInterfaceA = Mockito.mock(InterfaceA.class);
+
+        assertTrue(mockInterfaceA instanceof InterfaceA);
     }
 
     static class ClassA {}
