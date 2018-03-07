0490dcbabc12989bd4d98251040a07103a306909
tball <tball@google.com>
2018-03-07 11:49:12 -0800

    Issue #949: fixed reference cycles in Xalan's NamespaceMappings.
    	Change on 2018/03/07 by tball <tball@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=188210348

--- xalan/third_party/android/platform/external/apache-xml/src/main/java/org/apache/xml/serializer/NamespaceMappings.java
+++ xalan/third_party/android/platform/external/apache-xml/src/main/java/org/apache/xml/serializer/NamespaceMappings.java
@@ -30,7 +30,7 @@ import org.xml.sax.SAXException;
  * This class keeps track of the currently defined namespaces. Conceptually the
  * prefix/uri/depth triplets are pushed on a stack pushed on a stack. The depth
  * indicates the nesting depth of the element for which the mapping was made.
- * 
+ *
  * <p>For example:
  * <pre>
  * <chapter xmlns:p1="def">
@@ -42,7 +42,7 @@ import org.xml.sax.SAXException;
  *    </paragraph>
  * </chapter>
  * </pre>
- * 
+ *
  * When the <chapter> element is encounted the prefix "p1" associated with uri
  * "def" is pushed on the stack with depth 1.
  * When the first <paragraph> is encountered "p2" and "ghi" are pushed with
@@ -51,14 +51,14 @@ import org.xml.sax.SAXException;
  * When </sentance> occurs the popNamespaces(3) will pop "p3"/"jkl" off the
  * stack.  Of course popNamespaces(2) would pop anything with depth 2 or
  * greater.
- * 
+ *
  * So prefix/uri pairs are pushed and poped off the stack as elements are
  * processed.  At any given moment of processing the currently visible prefixes
  * are on the stack and a prefix can be found given a uri, or a uri can be found
  * given a prefix.
  *
  * This class is intended for internal use only.  However, it is made public because
- * other packages require it. 
+ * other packages require it.
  * @xsl.usage internal
  */
 public class NamespaceMappings
@@ -79,16 +79,16 @@ public class NamespaceMappings
      */
     private Hashtable m_namespaces = new Hashtable();
 
-    /** 
+    /**
      * This stack is used as a convenience.
      * It contains the pushed NamespaceNodes (shallowest
-     * to deepest) and is used to delete NamespaceNodes 
-     * when leaving the current element depth 
+     * to deepest) and is used to delete NamespaceNodes
+     * when leaving the current element depth
      * to returning to the parent. The mappings of the deepest
      * depth can be popped of the top and the same node
      * can be removed from the appropriate prefix stack.
-     * 
-     * All prefixes pushed at the current depth can be 
+     *
+     * All prefixes pushed at the current depth can be
      * removed at the same time by using this stack to
      * ensure prefix/uri map scopes are closed correctly.
      */
@@ -112,9 +112,9 @@ public class NamespaceMappings
      */
     private void initNamespaces()
     {
-        // The initial prefix mappings will never be deleted because they are at element depth -1 
+        // The initial prefix mappings will never be deleted because they are at element depth -1
         // (a kludge)
-        
+
         // Define the default namespace (initially maps to "" uri)
         Stack stack;
         MappingRecord nn;
@@ -130,7 +130,7 @@ public class NamespaceMappings
 
     /**
      * Use a namespace prefix to lookup a namespace URI.
-     * 
+     *
      * @param prefix String the prefix of the namespace
      * @return the URI corresponding to the prefix, returns ""
      * if there is no visible mapping.
@@ -146,18 +146,18 @@ public class NamespaceMappings
             uri = EMPTYSTRING;
         return uri;
     }
-  
-    
+
+
     MappingRecord getMappingFromPrefix(String prefix) {
         final Stack stack = (Stack) m_namespaces.get(prefix);
-        return stack != null && !stack.isEmpty() ? 
+        return stack != null && !stack.isEmpty() ?
             ((MappingRecord) stack.peek()) : null;
     }
 
     /**
-     * Given a namespace uri, and the namespaces mappings for the 
+     * Given a namespace uri, and the namespaces mappings for the
      * current element, return the current prefix for that uri.
-     * 
+     *
      * @param uri the namespace URI to be search for
      * @return an existing prefix that maps to the given URI, null if no prefix
      * maps to the given namespace URI.
@@ -178,7 +178,7 @@ public class NamespaceMappings
         }
         return foundPrefix;
     }
-    
+
     MappingRecord getMappingFromURI(String uri)
     {
         MappingRecord foundMap = null;
@@ -244,7 +244,7 @@ public class NamespaceMappings
                 // If the same prefix/uri mapping is already on the stack
                 // don't push this one.
                 // Or if we have a mapping at the same depth
-                // don't replace by pushing this one. 
+                // don't replace by pushing this one.
                 return false;
             }
         }
@@ -296,7 +296,7 @@ public class NamespaceMappings
                 // xmlns:x="abc" from the depth of element <b>
                 // when going back to <a> if in fact they have
                 // not been optimized away.
-                // 
+                //
                 prefixStack.pop();
                 if (saxHandler != null)
                 {
@@ -323,38 +323,38 @@ public class NamespaceMappings
         return "ns" + (count++);
     }
 
- 
+
     /**
      * This method makes a clone of this object.
      *
      */
     public Object clone() throws CloneNotSupportedException {
         NamespaceMappings clone = new NamespaceMappings();
-        clone.m_nodeStack = (NamespaceMappings.Stack) m_nodeStack.clone();        
+        clone.m_nodeStack = (NamespaceMappings.Stack) m_nodeStack.clone();
         clone.count = this.count;
         clone.m_namespaces = (Hashtable) m_namespaces.clone();
-        
+
         clone.count = count;
         return clone;
-        
+
     }
-    
+
     final void reset()
     {
         this.count = 0;
         this.m_namespaces.clear();
-        this.m_nodeStack.clear();        
-        
+        this.m_nodeStack.clear();
+
         initNamespaces();
     }
-    
+
     /**
      * Just a little class that ties the 3 fields together
      * into one object, and this simplifies the pushing
      * and popping of namespaces to one push or one pop on
      * one stack rather than on 3 separate stacks.
      */
-    class MappingRecord {
+    static class MappingRecord {
         final String m_prefix;  // the prefix
         final String m_uri;     // the uri, possibly "" but never null
         // the depth of the element where declartion was made
@@ -364,20 +364,20 @@ public class NamespaceMappings
             m_uri = (uri==null)? EMPTYSTRING : uri;
             m_declarationDepth = depth;
         }
-    }    
-    
+    }
+
     /**
      * Rather than using java.util.Stack, this private class
      * provides a minimal subset of methods and is faster
      * because it is not thread-safe.
      */
-    private class Stack {
+    private static class Stack {
         private int top = -1;
         private int max = 20;
         Object[] m_stack = new Object[max];
-        
+
         public Object clone() throws CloneNotSupportedException {
-            NamespaceMappings.Stack clone = new NamespaceMappings.Stack();  
+            NamespaceMappings.Stack clone = new NamespaceMappings.Stack();
             clone.max = this.max;
             clone.top = this.top;
             clone.m_stack = new Object[clone.max];
@@ -386,13 +386,13 @@ public class NamespaceMappings
             	// so it is OK if the clone has references to these.
             	clone.m_stack[i] = this.m_stack[i];
             }
-            return clone;            
+            return clone;
         }
-        
+
         public Stack()
         {
         }
-        
+
         public Object push(Object o) {
             top++;
             if (max <= top) {
@@ -405,7 +405,7 @@ public class NamespaceMappings
             m_stack[top] = o;
             return o;
         }
-        
+
         public Object pop() {
             Object o;
             if (0 <= top) {
@@ -417,7 +417,7 @@ public class NamespaceMappings
                 o = null;
             return o;
         }
-        
+
         public Object peek() {
             Object o;
             if (0 <= top) {
@@ -427,26 +427,26 @@ public class NamespaceMappings
                 o = null;
             return o;
         }
-        
+
         public Object peek(int idx) {
             return m_stack[idx];
         }
-        
+
         public boolean isEmpty() {
             return (top < 0);
         }
         public boolean empty() {
             return (top < 0);
         }
-        
+
         public void clear() {
             for (int i=0; i<= top; i++)
                 m_stack[i] = null;
             top = -1;
-        }  
-        
+        }
+
         public Object getElement(int index) {
-            return m_stack[index];      
+            return m_stack[index];
         }
     }
     /**
@@ -459,7 +459,7 @@ public class NamespaceMappings
         Stack fs = (Stack) m_namespaces.get(prefix);
         return fs;
     }
-    
+
     /**
      * A more type-safe way of saving stacks under the
      * m_namespaces Hashtable.
@@ -470,10 +470,10 @@ public class NamespaceMappings
         m_namespaces.put(prefix, fs);
         return fs;
     }
-    
+
     /**
-     * Given a namespace uri, get all prefixes bound to the Namespace URI in the current scope. 
-     * 
+     * Given a namespace uri, get all prefixes bound to the Namespace URI in the current scope.
+     *
      * @param uri the namespace URI to be search for
      * @return An array of Strings which are
      * all prefixes bound to the namespace URI in the current scope.
