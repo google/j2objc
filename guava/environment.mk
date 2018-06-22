b2bec658f114cad807f3ea035374c21340a57a97
antoniocortes <antoniocortes@google.com>
2018-06-22 14:36:27 -0700

    Refactoring Guava Makefile to use translate.mk
    	Change on 2018/06/22 by antoniocortes <antoniocortes@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=201743210

--- guava/environment.mk
+++ guava/environment.mk
@@ -20,14 +20,11 @@ include ../make/common.mk
 include ../make/j2objc_deps.mk
 include ../java_deps/jars.mk
 
-BUNDLE_SRC_DIR = $(BUILD_DIR)/bundle
-BUNDLE_SRC_JAR = $(BUILD_DIR)/guava-bundle-sources.jar
 GUAVA_SRC_JAR = $(JAVA_DEPS_JAR_DIR)/$(GUAVA_SOURCE_JAR)
 ERROR_PRONE_ANNOTATIONS_SRC_JAR = $(JAVA_DEPS_JAR_DIR)/$(ERROR_PRONE_ANNOTATIONS_SOURCE_JAR)
 CHECKER_QUAL_SRC_JAR = $(JAVA_DEPS_JAR_DIR)/$(CHECKER_QUAL_SOURCE_JAR)
 ANIMAL_SNIFFER_ANNOTATIONS_SRC_JAR = $(JAVA_DEPS_JAR_DIR)/$(ANIMAL_SNIFFER_ANNOTATIONS_SOURCE_JAR)
 
-# TODO(user): create a class bundle similar to the source bundle.
 JAR = $(JAVA_DEPS_JAR_DIR)/$(GUAVA_JAR)
 
 OBJS_DIR = $(BUILD_DIR)/objs
