db0794aa59c90581384cbe81ed88e71d936ffb7b
antoniocortes <antoniocortes@google.com>
2019-02-27 15:06:16 -0800

    JDK 11: updating instructions to patch jarjar.
    	Change on 2019/02/27 by antoniocortes <antoniocortes@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=236000590

---
+++ java_deps/jarjar/update_patched_jar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+cd jarjar-1.4-patched/
+ant
+cp dist/jarjar-1.4.jar ..
+cd ..
+diff -x '*.jar' -x 'build' -x 'dist' -ur jarjar-1.4 jarjar-1.4-patched > jarjar.patch
