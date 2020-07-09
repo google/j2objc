d3b5e72ed08d7d4bba67600096eddec401a7f5b5
albertomisail <amisail@google.com>
2020-07-09 16:45:27 -0700

    Adding StandardWatchEventKinds to build

--- jre_emul/jre_sources.mk
+++ jre_emul/jre_sources.mk
@@ -1091,6 +1091,7 @@ JAVA_PUBLIC_SOURCES_FILE = \
   java/nio/file/SimpleFileVisitor.java \
   java/nio/file/StandardCopyOption.java \
   java/nio/file/StandardOpenOption.java \
+  java/nio/file/StandardWatchEventKinds.java \
   java/nio/file/WatchEvent.java \
   java/nio/file/WatchKey.java \
   java/nio/file/WatchService.java \
