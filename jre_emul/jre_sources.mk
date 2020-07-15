3f230ff00b20357f721fbcbaf6b410ca4cd881bb
albertomisail <amisail@google.com>
2020-07-15 10:26:39 -0700

    Adding stub AbstractFileSystemProvider

--- jre_emul/jre_sources.mk
+++ jre_emul/jre_sources.mk
@@ -1128,6 +1128,7 @@ JAVA_PRIVATE_SOURCES_FILE = \
   sun/misc/JavaIOFileDescriptorAccess.java \
   sun/misc/SharedSecrets.java \
   sun/nio/fs/AbstractBasicFileAttributeView.java \
+  sun/nio/fs/AbstractFileSystemProvider.java \
   sun/nio/fs/AbstractFileTypeDetector.java \
   sun/nio/fs/AbstractPath.java \
   sun/nio/fs/AbstractWatchKey.java \
