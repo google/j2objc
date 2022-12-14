a2568da5818e3b5476e67979884e3c9a7663daca
J2ObjC Team <j2objc-copybara@google.com>
2022-12-14 15:48:39 -0800

    Update J2objC javax.annotation.processing.Generated to android13-release
    PiperOrigin-RevId: 495438869

--- jre_emul/Classes/javax/annotation/processing/Generated.java
+++ jre_emul/Classes/javax/annotation/processing/Generated.java
@@ -21,8 +21,25 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * javax.annotation.processing Generated annotation.
- * No code was referenced, created using just its public API.
+ * The Generated annotation is used to mark source code that has been generated. It can also be used
+ * to differentiate user written code from generated code in a single file.
+ *
+ * <h3>Examples:</h3>
+ *
+ * <pre>
+ *   &#064;Generated("com.example.Generator")
+ * </pre>
+ *
+ * <pre>
+ *   &#064;Generated(value="com.example.Generator", date= "2017-07-04T12:08:56.235-0700")
+ * </pre>
+ *
+ * <pre>
+ *   &#064;Generated(value="com.example.Generator", date= "2017-07-04T12:08:56.235-0700",
+ *      comments= "comment 1")
+ * </pre>
+ *
+ * @since 9
  */
 @Documented
 @Retention(value = RetentionPolicy.SOURCE)
@@ -38,11 +55,28 @@ import java.lang.annotation.Target;
     })
 public @interface Generated {
 
-  // Required elements.
+  /**
+   * The value element MUST have the name of the code generator. The name is the fully qualified
+   * name of the code generator.
+   *
+   * @return The name of the code generator
+   */
   String[] value();
 
-  // Optional elements.
+  /**
+   * Date when the source was generated. The date element must follow the ISO 8601 standard. For
+   * example the date element would have the following value 2017-07-04T12:08:56.235-0700 which
+   * represents 2017-07-04 12:08:56 local time in the U.S. Pacific Time time zone.
+   *
+   * @return The date the source was generated
+   */
   String comments() default "";
 
+  /**
+   * A place holder for any comments that the code generator may want to include in the generated
+   * code.
+   *
+   * @return Comments that the code generated included
+   */
   String date() default "";
 }
