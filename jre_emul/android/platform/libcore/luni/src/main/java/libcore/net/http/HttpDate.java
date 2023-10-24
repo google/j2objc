672d9f4fbbce5df24df36c88719b276ffa3202f1
J2ObjC Team <j2objc-copybara@google.com>
2023-10-24 15:32:33 -0700

    Undo performance optimization around standard data format.
    Eagerly creating the standard date format, and then re-using as much as possible, is purely a performance optimization that can possibly lead to issues due to the stateful-ness of DateFormat.
    
    In this cl we remove this performance optimization to simplify the code and avoid any possible state issues in DateFormat by creating a new parser on every invocation. This is similar to how the fallback formats are already handled anyway.
    
    PiperOrigin-RevId: 576304654

--- jre_emul/android/platform/libcore/luni/src/main/java/libcore/net/http/HttpDate.java
+++ jre_emul/android/platform/libcore/luni/src/main/java/libcore/net/http/HttpDate.java
@@ -28,64 +28,68 @@ import java.util.TimeZone;
  */
 public final class HttpDate {
 
-    /**
-     * Most websites serve cookies in the blessed format. Eagerly create the parser to ensure such
-     * cookies are on the fast path.
-     */
-    private static final ThreadLocal<DateFormat> STANDARD_DATE_FORMAT
-            = new ThreadLocal<DateFormat>() {
-        @Override protected DateFormat initialValue() {
-            DateFormat rfc1123 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
-            rfc1123.setTimeZone(TimeZone.getTimeZone("UTC"));
-            return rfc1123;
-        }
-    };
+  /**
+   * Most websites serve cookies in the blessed format. Eagerly create the parser to ensure such
+   * cookies are on the fast path.
+   */
+  // j2objc: Simplifying to avoid possible formatter state issues. Similar to Java 11 JRE.
+  // private static final ThreadLocal<DateFormat> STANDARD_DATE_FORMAT
+  //         = new ThreadLocal<DateFormat>() {
+  //     @Override protected DateFormat initialValue() {
+  //         DateFormat rfc1123 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
+  //         rfc1123.setTimeZone(TimeZone.getTimeZone("UTC"));
+  //         return rfc1123;
+  //     }
+  // };
+  private static final String STANDARD_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
 
-    /**
-     * If we fail to parse a date in a non-standard format, try each of these formats in sequence.
-     */
-    private static final String[] BROWSER_COMPATIBLE_DATE_FORMATS = new String[] {
-            /* This list comes from  {@code org.apache.http.impl.cookie.BrowserCompatSpec}. */
-            "EEEE, dd-MMM-yy HH:mm:ss zzz", // RFC 1036
-            "EEE MMM d HH:mm:ss yyyy", // ANSI C asctime()
-            "EEE, dd-MMM-yyyy HH:mm:ss z",
-            "EEE, dd-MMM-yyyy HH-mm-ss z",
-            "EEE, dd MMM yy HH:mm:ss z",
-            "EEE dd-MMM-yyyy HH:mm:ss z",
-            "EEE dd MMM yyyy HH:mm:ss z",
-            "EEE dd-MMM-yyyy HH-mm-ss z",
-            "EEE dd-MMM-yy HH:mm:ss z",
-            "EEE dd MMM yy HH:mm:ss z",
-            "EEE,dd-MMM-yy HH:mm:ss z",
-            "EEE,dd-MMM-yyyy HH:mm:ss z",
-            "EEE, dd-MM-yyyy HH:mm:ss z",
+  private static DateFormat createStandardDateFormatParser() {
+    DateFormat rfc1123 = new SimpleDateFormat(STANDARD_DATE_FORMAT, Locale.US);
+    rfc1123.setTimeZone(TimeZone.getTimeZone("UTC"));
+    return rfc1123;
+  }
 
-            /* RI bug 6641315 claims a cookie of this format was once served by www.yahoo.com */
-            "EEE MMM d yyyy HH:mm:ss z",
-    };
+  // End j2objc change.
 
-    /**
-     * Returns the date for {@code value}. Returns null if the value couldn't be
-     * parsed.
-     */
-    public static Date parse(String value) {
-        try {
-            return STANDARD_DATE_FORMAT.get().parse(value);
-        } catch (ParseException ignore) {
-        }
-        for (String formatString : BROWSER_COMPATIBLE_DATE_FORMATS) {
-            try {
-                return new SimpleDateFormat(formatString, Locale.US).parse(value);
-            } catch (ParseException ignore) {
-            }
-        }
-        return null;
+  /** If we fail to parse a date in a non-standard format, try each of these formats in sequence. */
+  private static final String[] BROWSER_COMPATIBLE_DATE_FORMATS =
+      new String[] {
+        /* This list comes from  {@code org.apache.http.impl.cookie.BrowserCompatSpec}. */
+        "EEEE, dd-MMM-yy HH:mm:ss zzz", // RFC 1036
+        "EEE MMM d HH:mm:ss yyyy", // ANSI C asctime()
+        "EEE, dd-MMM-yyyy HH:mm:ss z",
+        "EEE, dd-MMM-yyyy HH-mm-ss z",
+        "EEE, dd MMM yy HH:mm:ss z",
+        "EEE dd-MMM-yyyy HH:mm:ss z",
+        "EEE dd MMM yyyy HH:mm:ss z",
+        "EEE dd-MMM-yyyy HH-mm-ss z",
+        "EEE dd-MMM-yy HH:mm:ss z",
+        "EEE dd MMM yy HH:mm:ss z",
+        "EEE,dd-MMM-yy HH:mm:ss z",
+        "EEE,dd-MMM-yyyy HH:mm:ss z",
+        "EEE, dd-MM-yyyy HH:mm:ss z",
+
+        /* RI bug 6641315 claims a cookie of this format was once served by www.yahoo.com */
+        "EEE MMM d yyyy HH:mm:ss z",
+      };
+
+  /** Returns the date for {@code value}. Returns null if the value couldn't be parsed. */
+  public static Date parse(String value) {
+    try {
+      return createStandardDateFormatParser().parse(value);
+    } catch (ParseException ignore) {
     }
-
-    /**
-     * Returns the string for {@code value}.
-     */
-    public static String format(Date value) {
-        return STANDARD_DATE_FORMAT.get().format(value);
+    for (String formatString : BROWSER_COMPATIBLE_DATE_FORMATS) {
+      try {
+        return new SimpleDateFormat(formatString, Locale.US).parse(value);
+      } catch (ParseException ignore) {
+      }
     }
+    return null;
+  }
+
+  /** Returns the string for {@code value}. */
+  public static String format(Date value) {
+    return createStandardDateFormatParser().format(value);
+  }
 }
