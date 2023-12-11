9485be98febd11be39c21f184e9ea4ee49fdbc4c
Yuxi Sun <yuxisun@google.com>
2023-12-10 20:36:52 -0800

    Enable DateFormatSymbolsTest on JRE.
    PiperOrigin-RevId: 589691089

--- jre_emul/test_sources.mk
+++ jre_emul/test_sources.mk
@@ -269,7 +269,6 @@ NATIVE_SOURCES = dalvik_system_JniTest.cpp
 # com/google/j2objc/java8/LambdaTest.java \
 # com/google/j2objc/java8/SuperMethodReferenceTest.java \
 # java/nio/file/attribute/FileTimeTest.java \
-# org/apache/harmony/tests/java/text/DateFormatSymbolsTest.java \
 # tck/java/time/format/TCKZoneIdPrinterParser.java \
 # test/java/time/format/TestDateTimeFormatterBuilder.java \
 # test/java/time/format/TestZoneOffsetParser.java \
@@ -862,6 +861,7 @@ TEST_SOURCES := \
     org/apache/harmony/tests/java/io/ObjectStreamConstantsTest.java \
     org/apache/harmony/tests/java/io/ObjectStreamFieldTest.java \
     org/apache/harmony/tests/java/io/RandomAccessFileTest.java \
+    org/apache/harmony/tests/java/text/DateFormatSymbolsTest.java \
     org/apache/harmony/tests/java/text/SimpleDateFormatTest.java \
     org/apache/harmony/tests/java/lang/AbstractMethodErrorTest.java \
     org/apache/harmony/tests/java/lang/ArithmeticExceptionTest.java \
