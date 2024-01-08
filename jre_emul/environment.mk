bc05d5b643b5e76213ab2643b0d35709f033a8b5
Tom Ball <tball@google.com>
2024-01-08 15:51:49 -0800

    Suppress new clang warning (-Wbitwise-instead-of-logical) in JRE build. This is safe because it's allowed in the Java language spec.
    PiperOrigin-RevId: 596725095

--- jre_emul/environment.mk
+++ jre_emul/environment.mk
@@ -146,7 +146,8 @@ NATIVE_SOURCE_DIRS = $(EMULATION_CLASS_DIR) $(APPLE_ROOT) $(ANDROID_NATIVE_DIR) 
 
 # Clang warnings
 WARNINGS := $(CC_WARNINGS) $(WARNINGS) -Wno-unused-label -Wno-dangling-else \
-  -Wimplicit-function-declaration -Wint-conversion -Wshorten-64-to-32 -Wcomma -Wnonnull
+  -Wimplicit-function-declaration -Wint-conversion -Wshorten-64-to-32 -Wcomma \
+  -Wnonnull -Wno-bitwise-instead-of-logical
 
 ifeq ("$(strip $(XCODE_VERSION_MAJOR))", "0500")
 OBJCFLAGS += -DSET_MIN_IOS_VERSION
