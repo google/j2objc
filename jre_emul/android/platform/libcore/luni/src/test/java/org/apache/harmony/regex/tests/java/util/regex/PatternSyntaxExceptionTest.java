7070572a5f616d91b38a3722bad1378a23db1f7c
Yuxi Sun <yuxisun@google.com>
2024-01-31 11:08:39 -0800

    Enable tests in PatternSyntaxExceptionTest
    PiperOrigin-RevId: 603104019

--- jre_emul/android/platform/libcore/luni/src/test/java/org/apache/harmony/regex/tests/java/util/regex/PatternSyntaxExceptionTest.java
+++ jre_emul/android/platform/libcore/luni/src/test/java/org/apache/harmony/regex/tests/java/util/regex/PatternSyntaxExceptionTest.java
@@ -26,68 +26,68 @@ import org.apache.harmony.testframework.serialization.SerializationTest;
 import org.apache.harmony.testframework.serialization.SerializationTest.SerializableAssert;
 
 public class PatternSyntaxExceptionTest extends TestCase {
-  // TODO(b/265202484):fix the JRE test error
-  // public void testPatternSyntaxException() {
-  //   // Normal case
-  //   PatternSyntaxException e = new PatternSyntaxException("Foo", "Bar", 0);
-  //   assertEquals("Foo", e.getDescription());
-  //   assertEquals("Bar", e.getPattern());
-  //   assertEquals(0, e.getIndex());
+  public void testPatternSyntaxException() {
+    // Normal case
+    PatternSyntaxException e = new PatternSyntaxException("Foo", "Bar", 0);
+    assertEquals("Foo", e.getDescription());
+    assertEquals("Bar", e.getPattern());
+    assertEquals(0, e.getIndex());
 
-  //   String s = e.getMessage();
-  //   assertTrue(s.contains("Foo"));
-  //   assertTrue(s.contains("Bar"));
-  //   assertTrue(s.contains("0"));
+    String s = e.getMessage();
+    assertTrue(s.contains("Foo"));
+    assertTrue(s.contains("Bar"));
+    assertTrue(s.contains("0"));
+    assertTrue(s.contains("^"));
 
-  //   // No description specified
-  //   e = new PatternSyntaxException(null, "Bar", 0);
-  //   assertEquals(null, e.getDescription());
-  //   assertEquals("Bar", e.getPattern());
-  //   assertEquals(0, e.getIndex());
+    // No description specified
+    e = new PatternSyntaxException(null, "Bar", 0);
+    assertEquals(null, e.getDescription());
+    assertEquals("Bar", e.getPattern());
+    assertEquals(0, e.getIndex());
 
-  //   s = e.getMessage();
-  //   assertFalse(s.contains("Foo"));
-  //   assertTrue(s.contains("Bar"));
-  //   assertTrue(s.contains("0"));
+    s = e.getMessage();
+    assertFalse(s.contains("Foo"));
+    assertTrue(s.contains("Bar"));
+    assertTrue(s.contains("0"));
+    assertTrue(s.contains("^"));
 
-  //   // No pattern specified
-  //   e = new PatternSyntaxException("Foo", null, 0);
-  //   assertEquals("Foo", e.getDescription());
-  //   assertEquals(null, e.getPattern());
-  //   assertEquals(0, e.getIndex());
+    // No pattern specified
+    e = new PatternSyntaxException("Foo", null, 0);
+    assertEquals("Foo", e.getDescription());
+    assertEquals(null, e.getPattern());
+    assertEquals(0, e.getIndex());
 
-  //   s = e.getMessage();
-  //   assertTrue(s.contains("Foo"));
-  //   assertFalse(s.contains("Bar"));
-  //   assertTrue(s.contains("0"));
+    s = e.getMessage();
+    assertTrue(s.contains("Foo"));
+    assertFalse(s.contains("Bar"));
+    assertTrue(s.contains("0"));
 
-  //   // Neither description nor pattern specified
-  //   e = new PatternSyntaxException(null, null, 0);
-  //   assertEquals(null, e.getDescription());
-  //   assertEquals(null, e.getPattern());
-  //   assertEquals(0, e.getIndex());
+    // Neither description nor pattern specified
+    e = new PatternSyntaxException(null, null, 0);
+    assertEquals(null, e.getDescription());
+    assertEquals(null, e.getPattern());
+    assertEquals(0, e.getIndex());
 
-  //   s = e.getMessage();
-  //   assertFalse(s.contains("Foo"));
-  //   assertFalse(s.contains("Bar"));
-  //   assertTrue(s.contains("0"));
+    s = e.getMessage();
+    assertFalse(s.contains("Foo"));
+    assertFalse(s.contains("Bar"));
+    assertTrue(s.contains("0"));
 
-  //   // No index specified
-  //   e = new PatternSyntaxException("Foo", "Bar", -1);
-  //   assertEquals(-1, e.getIndex());
+    // No index specified
+    e = new PatternSyntaxException("Foo", "Bar", -1);
+    assertEquals(-1, e.getIndex());
 
-  //   s = e.getMessage();
-  //   assertFalse(s.contains("^"));
+    s = e.getMessage();
+    assertFalse(s.contains("^"));
 
-  //   // No pattern, but index specified. NOTE: This is an "unusual" case since
-  //   // it make no sense to provide an index and not a pattern.
-  //   e = new PatternSyntaxException("Foo", null, 0);
-  //   assertEquals(0, e.getIndex());
+    // No pattern, but index specified. NOTE: This is an "unusual" case since
+    // it make no sense to provide an index and not a pattern.
+    e = new PatternSyntaxException("Foo", null, 0);
+    assertEquals(0, e.getIndex());
 
-  //   s = e.getMessage();
-  //   assertTrue(s.contains("^"));
-  //   assertTrue(s.contains("null"));
-  // }
+    s = e.getMessage();
+    assertTrue(s.contains("null"));
+  }
 
   public void testCase() {
     String regex = "(";
