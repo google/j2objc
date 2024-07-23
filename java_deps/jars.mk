e582dc9d841712ee43f1bdf8a64c6ffc1c0a7cbe
Tom Ball <tball@google.com>
2024-07-23 13:54:23 -0700

    Updates protobuf-java jars to v4.27.2 for public build.
    PiperOrigin-RevId: 655292393

--- java_deps/jars.mk
+++ java_deps/jars.mk
@@ -30,8 +30,8 @@ JUNIT_DATAPROVIDER_SOURCE_JAR = junit-dataprovider-1.10.4-sources.jar
 HAMCREST_JAR = hamcrest-core-1.3.jar
 HAMCREST_SOURCE_JAR = hamcrest-core-1.3-sources.jar
 
-PROTOBUF_JAR = protobuf-java-3.25.2.jar
-PROTOBUF_SOURCE_JAR = protobuf-java-3.25.2-sources.jar
+PROTOBUF_JAR = protobuf-java-4.27.2.jar
+PROTOBUF_SOURCE_JAR = protobuf-java-4.27.2-sources.jar
 
 ERROR_PRONE_ANNOTATIONS_JAR = error_prone_annotations-2.11.0.jar
 ERROR_PRONE_ANNOTATIONS_SOURCE_JAR = error_prone_annotations-2.11.0-sources.jar
