b9658fcdd30245b31dd9f7873d4fcec7d5492a60
Tom Ball <tball@google.com>
2024-09-04 14:54:10 -0700

    Issue #2332: fix bridging header for HelloSwift example. This demonstrates the correct way to include j2objc-generated headers into a Swift application.
    PiperOrigin-RevId: 671109904

--- examples/HelloSwift/HelloSwift/HelloSwift-Bridging-Header.h
+++ examples/HelloSwift/HelloSwift/HelloSwift-Bridging-Header.h
@@ -1,14 +1,19 @@
-#import "org/j2objc/Example.h"
-#import "java/lang/Boolean.h"
-#import "java/lang/Float.h"
-#import "java/lang/Long.h"
-#import "java/lang/StringBuilder.h"
-#import "java/text/NumberFormat.h"
-#import "java/util/ArrayList.h"
-#import "java/util/Calendar.h"
-#import "java/util/Date.h"
-#import "java/util/Enumeration.h"
-#import "java/util/Iterator.h"
-#import "java/util/List.h"
-#import "java/util/Vector.h"
-#import "java/util/regex/Pattern.h"
+#ifndef HELLO_SWIFT_BRIDGING_HEADER_H_
+#define HELLO_SWIFT_BRIDGING_HEADER_H_
+
+#include "org/j2objc/Example.h"
+#include "java/lang/Boolean.h"
+#include "java/lang/Float.h"
+#include "java/lang/Long.h"
+#include "java/lang/StringBuilder.h"
+#include "java/text/NumberFormat.h"
+#include "java/util/ArrayList.h"
+#include "java/util/Calendar.h"
+#include "java/util/Date.h"
+#include "java/util/Enumeration.h"
+#include "java/util/Iterator.h"
+#include "java/util/List.h"
+#include "java/util/Vector.h"
+#include "java/util/regex/Pattern.h"
+
+#endif  // HELLO_SWIFT_BRIDGING_HEADER_H_
