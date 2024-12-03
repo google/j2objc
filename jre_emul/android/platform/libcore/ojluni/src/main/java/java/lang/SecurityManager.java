18be959bdb90e5960862f2a34015955f3dc7e41e
Liam Miller-Cushon <cushon@google.com>
2024-12-02 18:35:59 -0800

    Internal change
    PiperOrigin-RevId: 702154975

--- jre_emul/android/platform/libcore/ojluni/src/main/java/java/lang/SecurityManager.java
+++ jre_emul/android/platform/libcore/ojluni/src/main/java/java/lang/SecurityManager.java
@@ -43,6 +43,8 @@ import java.net.InetAddress;
  * developers can assume that there's no SecurityManager installed,
  * i.e. {@link java.lang.System#getSecurityManager()} will return null.
  */
+// TODO: b/381944419 - remove SecurityManager usage
+@com.google.devtools.staticanalysis.errorprone.SuppressBanSecurityManager
 public
 class SecurityManager {
 
