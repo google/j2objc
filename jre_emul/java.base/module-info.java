d1570cc5b386616a10e93bf99e13d013f302a2f3
Tom Ball <tball@google.com>
2025-06-16 21:12:34 -0700

    Bazel build support for its Java targets: annotations jar, translator, and jre_emul's jre_emul.jar.
    This requires Bazel be installed, it's recommended that be done using "brew update && brew install bazel bazelisk".
    
    PiperOrigin-RevId: 772292685

---
+++ jre_emul/java.base/module-info.java
@@ -0,0 +1,119 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+module java.base {
+  exports com.google.j2objc;
+  exports java.awt.font;
+  exports java.beans;
+  exports java.io;
+  exports java.lang;
+  exports java.lang.annotation;
+  exports java.lang.invoke;
+  exports java.lang.management;
+  exports java.lang.ref;
+  exports java.lang.reflect;
+  exports java.math;
+  exports java.net;
+  exports java.nio;
+  exports java.nio.channels;
+  exports java.nio.channels.spi;
+  exports java.nio.charset;
+  exports java.nio.charset.spi;
+  exports java.nio.file;
+  exports java.nio.file.attribute;
+  exports java.nio.file.spi;
+  exports java.security;
+  exports java.security.cert;
+  exports java.security.interfaces;
+  exports java.security.spec;
+  exports java.sql;
+  exports java.text;
+  exports java.time;
+  exports java.time.chrono;
+  exports java.time.format;
+  exports java.time.temporal;
+  exports java.time.zone;
+  exports java.util;
+  exports java.util.concurrent;
+  exports java.util.concurrent.atomic;
+  exports java.util.concurrent.locks;
+  exports java.util.function;
+  exports java.util.jar;
+  exports java.util.logging;
+  exports java.util.regex;
+  exports java.util.stream;
+  exports java.util.zip;
+  exports javax.annotation.processing;
+  exports javax.crypto;
+  exports javax.crypto.interfaces;
+  exports javax.crypto.spec;
+  exports javax.lang.model.element;
+  exports javax.lang.model.type;
+  exports javax.net;
+  exports javax.net.ssl;
+  exports javax.security.auth;
+  exports javax.security.auth.callback;
+  exports javax.security.auth.x500;
+  exports javax.security.cert;
+  exports javax.sql;
+  exports javax.xml;
+  exports javax.xml.datatype;
+  exports javax.xml.namespace;
+  exports javax.xml.parsers;
+  exports javax.xml.transform;
+  exports javax.xml.transform.dom;
+  exports javax.xml.transform.sax;
+  exports javax.xml.transform.stream;
+  exports javax.xml.validation;
+  exports javax.xml.xpath;
+  exports org.apache.harmony.beans;
+  exports org.apache.harmony.xml.dom;
+  exports org.apache.harmony.xml.parsers;
+  exports org.kxml2.io;
+  exports org.w3c.dom;
+  exports org.w3c.dom.ls;
+  exports org.w3c.dom.traversal;
+  exports org.xml.sax;
+  exports org.xml.sax.ext;
+  exports org.xml.sax.helpers;
+  exports org.xmlpull.v1;
+  exports org.xmlpull.v1.sax2;
+  exports sun.misc;
+  exports sun.net;
+  exports sun.net.spi.nameservice;
+  exports sun.net.util;
+  exports sun.net.www;
+  exports sun.net.www.protocol.file;
+  exports sun.nio.ch;
+  exports sun.nio.cs;
+  exports sun.nio.fs;
+  exports sun.reflect;
+  exports sun.reflect.misc;
+  exports sun.security.action;
+  exports sun.security.jca;
+  exports sun.security.pkcs;
+  exports sun.security.provider;
+  exports sun.security.provider.certpath;
+  exports sun.security.timestamp;
+  exports sun.security.util;
+  exports sun.security.x509;
+  exports sun.util;
+  exports sun.util.calendar;
+  exports sun.util.locale;
+  exports sun.util.locale.provider;
+  exports sun.util.logging;
+}
