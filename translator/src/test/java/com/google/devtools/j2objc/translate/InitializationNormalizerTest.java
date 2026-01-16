cb57e0bc2b2b42577a72166666c8d694f0c1fbdf
Roberto Lublinerman <rluble@google.com>
2026-01-15 17:14:42 -0800

    Cleanup in `InitializationNormalizerTest` in preparation to implement Java 25 flexible constructor bodies.
    PiperOrigin-RevId: 856883528

--- translator/src/test/java/com/google/devtools/j2objc/translate/InitializationNormalizerTest.java
+++ translator/src/test/java/com/google/devtools/j2objc/translate/InitializationNormalizerTest.java
@@ -32,16 +32,29 @@ public class InitializationNormalizerTest extends GenerationTest {
    * initialize self, rather than a super constructor call.
    */
   public void testThisConstructorCallInlined() throws IOException {
-    String source = "class Test {"
-        + "boolean b1; boolean b2;"
-        + "Test() { this(true); b2 = true; }"
-        + "Test(boolean b) { b1 = b; }}";
+    String source =
+        """
+        class Test {
+          boolean b1;
+          boolean b2;
+          Test() {
+            this(true);
+            b2 = true;
+          }
+          Test(boolean b) {
+            b1 = b;
+          }
+        }
+        """;
     String translation = translateSourceFile(source, "Test", "Test.m");
-    assertTranslatedLines(translation,
-        "void Test_init(Test *self) {",
-        "  Test_initWithBoolean_(self, true);",
-        "  self->b2_ = true;",
-        "}");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_init(Test *self) {
+          Test_initWithBoolean_(self, true);
+          self->b2_ = true;
+        }
+        """);
   }
 
   /**
@@ -51,102 +64,187 @@ public class InitializationNormalizerTest extends GenerationTest {
    * @throws IOException
    */
   public void testFieldArrayInitializer() throws IOException {
-    String source = "public class Distance {"
-        + "private class SimplexVertex {}"
-        + "private class Simplex {"
-        + "  public final SimplexVertex vertices[] = {"
-        + "    new SimplexVertex() "
-        + "  }; }}";
+    String source =
+        """
+        public class Distance {
+          private class SimplexVertex {}
+          private class Simplex {
+            public final SimplexVertex vertices[] = {
+              new SimplexVertex()
+            };
+          }
+        }
+        """;
     String translation = translateSourceFile(source, "Distance", "Distance.m");
     assertInTranslation(
         translation,
-        "[IOSObjectArray newArrayWithObjects:(id[]){ "
-            + "create_Distance_SimplexVertex_initWithDistance_(outer$) } "
-            + "count:1 type:Distance_SimplexVertex_class_()]");
+        """
+        [IOSObjectArray newArrayWithObjects:(id[]){\
+         create_Distance_SimplexVertex_initWithDistance_(outer$) \
+        } count:1 type:Distance_SimplexVertex_class_()]);
+        """);
   }
 
   public void testStaticVarInitialization() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { static java.util.Date date = new java.util.Date(); }", "Test", "Test.m");
+    String translation =
+        translateSourceFile(
+            """
+            class Test {
+              static java.util.Date date = new java.util.Date();
+             }
+            """,
+            "Test",
+            "Test.m");
     // test that initializer was stripped from the declaration
     assertInTranslation(translation, "JavaUtilDate *Test_date;");
     // test that initializer was moved to new initialize method
-    assertTranslatedLines(translation,
-        "+ (void)initialize {",
-        "if (self == [Test class]) {",
-        "JreStrongAssignAndConsume(&Test_date, new_JavaUtilDate_init());");
+    assertTranslatedLines(
+        translation,
+        """
+        + (void)initialize {
+          if (self == [Test class]) {
+            JreStrongAssignAndConsume(&Test_date, new_JavaUtilDate_init());
+            J2OBJC_SET_INITIALIZED(Test)
+          }
+        }
+        """);
   }
 
   public void testFieldInitializer() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { java.util.Date date = new java.util.Date(); }", "Test", "Test.m");
+    String translation =
+        translateSourceFile(
+            """
+            class Test {
+               java.util.Date date = new java.util.Date();
+             }
+            """,
+            "Test",
+            "Test.m");
     // Test that a default constructor was created and the initializer statement
     // moved to the constructor.
-    assertTranslatedLines(translation,
-        "void Test_init(Test *self) {",
-        "  NSObject_init(self);",
-        "  JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());",
-        "}");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_init(Test *self) {
+          NSObject_init(self);
+          JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());
+        }
+        """);
   }
 
   public void testInitializationBlock() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { java.util.Date date; { date = new java.util.Date(); } }", "Test", "Test.m");
+    String translation =
+        translateSourceFile(
+            """
+            class Test {
+              java.util.Date date;
+              {
+                date = new java.util.Date();
+              }
+            }
+            """,
+            "Test",
+            "Test.m");
     // Test that a default constructor was created and the initializer statement
     // moved to the constructor.
-    assertTranslatedLines(translation,
-        "void Test_init(Test *self) {",
-        "  NSObject_init(self);",
-        "  {",
-        "    JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());",
-        "  }",
-        "}");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_init(Test *self) {
+          NSObject_init(self);
+          {
+            JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());
+          }
+        }
+        """);
   }
 
   public void testStaticInitializerBlock() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { static { System.out.println(\"foo\"); } }", "Test", "Test.m");
+    String translation =
+        translateSourceFile(
+            """
+            class Test {
+              static {
+                System.out.println("foo");
+              }
+            }
+            """,
+            "Test",
+            "Test.m");
     // test that a static initialize() method was created and that it contains
     // the block's statement.
-    assertTranslatedLines(translation,
-        "+ (void)initialize {",
-        "if (self == [Test class]) {",
-        "{",
-        "[((JavaIoPrintStream *) nil_chk(JreLoadStatic(JavaLangSystem, out))) "
-            + "printlnWithNSString:@\"foo\"];");
+    assertTranslatedLines(
+        translation,
+        """
+        + (void)initialize {
+          if (self == [Test class]) {
+            {
+              [((JavaIoPrintStream *) nil_chk(JreLoadStatic(JavaLangSystem, out))) printlnWithNSString:@"foo"];
+            }
+            J2OBJC_SET_INITIALIZED(Test)
+          }
+        }
+        """);
   }
 
   public void testInitializerMovedToDesignatedConstructor() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { java.util.Date date; { date = new java.util.Date(); } "
-        + "public Test() { this(2); } public Test(int i) { System.out.println(i); } }",
-        "Test", "Test.m");
+    String source =
+        """
+        class Test {
+          java.util.Date date;
+          {
+            date = new java.util.Date();
+          }
+          public Test() {
+            this(2);
+          }
+          public Test(int i) {
+            System.out.println(i);
+          }
+        }
+        """;
+    String translation = translateSourceFile(source, "Test", "Test.m");
     // test that default constructor was untouched, since it calls self()
-    assertTranslatedLines(translation,
-        "void Test_init(Test *self) {",
-        "  Test_initWithInt_(self, 2);",
-        "}");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_init(Test *self) {
+          Test_initWithInt_(self, 2);
+        }
+        """);
     // test that initializer statement was added to second constructor
-    assertTranslatedLines(translation,
-        "void Test_initWithInt_(Test *self, int32_t i) {",
-        "  NSObject_init(self);",
-        "  {",
-        "    JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());",
-        "  }",
-        "  [((JavaIoPrintStream *) nil_chk(JreLoadStatic(JavaLangSystem, out))) "
-          + "printlnWithInt:i];",
-        "}");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_initWithInt_(Test *self, int32_t i) {
+          NSObject_init(self);
+          {
+            JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());
+          }
+          [((JavaIoPrintStream *) nil_chk(JreLoadStatic(JavaLangSystem, out))) printlnWithInt:i];
+        }
+        """);
   }
 
   public void testInitializerMovedToEmptyConstructor() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { java.util.Date date = new java.util.Date(); public Test() {} }",
-        "Test", "Test.m");
-    assertTranslatedLines(translation,
-        "void Test_init(Test *self) {",
-        "  NSObject_init(self);",
-        "  JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());",
-        "}");
+    String translation =
+        translateSourceFile(
+            """
+            class Test {
+              java.util.Date date = new java.util.Date();
+              public Test() {}
+            }
+            """,
+            "Test",
+            "Test.m");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_init(Test *self) {
+           NSObject_init(self);
+           JreStrongAssignAndConsume(&self->date_, new_JavaUtilDate_init());
+        }
+        """);
   }
 
   /**
@@ -154,24 +252,41 @@ public class InitializationNormalizerTest extends GenerationTest {
    * an interface has a constant defined.
    */
   public void testInterfaceConstantsIgnored() throws IOException {
-    String source = "public interface Mouse { int BUTTON_LEFT = 0; }";
+    String source =
+        """
+        public interface Mouse {
+          public static final int BUTTON_LEFT = 0;
+        }
+        """;
     String translation = translateSourceFile(source, "Mouse", "Mouse.h");
     assertInTranslation(translation, "#define Mouse_BUTTON_LEFT 0");
   }
 
   public void testStringWithInvalidCppCharacters() throws IOException {
-    String source = "class Test { static final String foo = \"\\udfff\"; }";
+    String source =
+        """
+        class Test {
+          static final String foo = "\udfff";
+        }
+        """;
     String translation = translateSourceFile(source, "Test", "Test.m");
     assertInTranslation(translation, "NSString *Test_foo;");
     assertInTranslation(
         translation,
-        "JreStrongAssign(&Test_foo, [NSString stringWithCharacters:(unichar[]) { "
-            + "(int) 0xdfff } length:1]);");
+        """
+        JreStrongAssign(&Test_foo, [NSString stringWithCharacters:(unichar[]) {\
+         (int) 0xdfff } length:1]);
+        """);
   }
 
   public void testStringConcatWithInvalidCppCharacters() throws IOException {
     // Include a 1 between the strings so javac's parser doesn't combine them.
-    String source = "class Test { static final String foo = \"hello\" + 1 + \"\\udfff\"; }";
+    String source =
+        """
+        class Test {
+          static final String foo = "hello" + 1 + "\udfff";
+        }
+        """;
     String translation = translateSourceFile(source, "Test", "Test.m");
     assertInTranslation(translation, "NSString *Test_foo;");
     assertInTranslation(
@@ -181,9 +296,18 @@ public class InitializationNormalizerTest extends GenerationTest {
   }
 
   public void testInitializersPlacedAfterOuterAssignments() throws IOException {
-    String source = "class Test { "
-         + "  int outerVar = 1; "
-         + "  class Inner { int innerVar = outerVar; void test() { outerVar++; } } }";
+    String source =
+        """
+        class Test {
+          int outerVar = 1;
+          class Inner {
+            int innerVar = outerVar;
+            void test() {
+              outerVar++;
+            }
+          }
+        }
+        """;
     String translation = translateSourceFile(source, "Test", "Test.m");
     assertInTranslation(translation, "JreStrongAssign(&self->this$0_, outer$);");
     assertInTranslation(translation, "innerVar_ = outer$->outerVar_;");
@@ -193,11 +317,16 @@ public class InitializationNormalizerTest extends GenerationTest {
 
   public void testStaticInitializersKeptInOrder() throws IOException {
     String source =
-        "public class Test { "
-        + "  public static final int I = 1; "
-        + "  public static final java.util.Set<Integer> iSet = new java.util.HashSet<Integer>(); "
-        + "  static { iSet.add(I); } "
-        + "  public static final int iSetSize = iSet.size(); }";
+        """
+        public class Test {
+          public static final int I = 1;
+          public static final java.util.Set<Integer> iSet = new java.util.HashSet<Integer>();
+          static {
+            iSet.add(I);
+          }
+          public static final int iSetSize = iSet.size();
+        }
+        """;
     String translation = translateSourceFile(source, "Test", "Test.m");
     String setInit = "JreStrongAssignAndConsume(&Test_iSet, new_JavaUtilHashSet_init())";
     String setAdd = "[Test_iSet addWithId:JavaLangInteger_valueOfWithInt_(Test_I)]";
@@ -210,16 +339,30 @@ public class InitializationNormalizerTest extends GenerationTest {
   }
 
   public void testStaticFinalStringAssignedToStaticFinalString() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { static final String FOO = Inner.BAR; "
-        + "class Inner { static final String BAR = \"bar\"; } }", "Test", "Test.m");
+    String source =
+        """
+        class Test {
+          static final String FOO = Inner.BAR;
+          class Inner {
+            static final String BAR = "bar";
+          }
+        }
+        """;
+    String translation = translateSourceFile(source, "Test", "Test.m");
     assertInTranslation(translation, "NSString *Test_FOO = @\"bar\";");
   }
 
   public void testVarargConstructorCallFromSubclass() throws IOException {
-    String translation = translateSourceFile(
-        "class A { A(Object ... bars) {} static class B extends A {}}",
-        "A", "A.m");
+    String translation =
+        translateSourceFile(
+            """
+            class A {
+               A(Object ... bars) {}
+               static class B extends A {}
+            }
+            """,
+            "A",
+            "A.m");
     assertNotInTranslation(translation, "A_init(self);");
     assertInTranslation(
         translation,
@@ -239,41 +382,71 @@ public class InitializationNormalizerTest extends GenerationTest {
     // Translate the file in the temp directory (i.e. avoid in-memory copy) because the temp
     // directory is already configured as a patch-module location.
     String filename = "java/lang/Object";
-    String path = addSourceFile("package java.lang;"
-            + "public class Object {"
-            + "  public Object() {}}",
-        filename + ".java");
+    String source =
+        """
+        package java.lang;
+        public class Object {
+          public Object() {}
+        }
+        """;
+    String path = addSourceFile(source, filename + ".java");
     String translation = translateSourceFileNoInMemory(path, filename + ".h");
     assertInTranslation(translation, "@interface NSObject");
   }
 
   // Regression test for Issue #809.
   public void testConstantInitializedByConditionalOnConstants() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { private static final int A = 1; private static final int B = 2; "
-        + "private static final int C = A < B ? A : B; }", "Test", "Test.m");
+    String source =
+        """
+        class Test {
+          private static final int A = 1;
+          private static final int B = 2;
+          private static final int C = A < B ? A : B;
+        }
+        """;
+    String translation = translateSourceFile(source, "Test", "Test.m");
     assertInTranslation(translation, "#define Test_C 1");
   }
 
   // Verify that code in a double-brace is added to initialize method.
   public void testDoubleBraceInitialization() throws IOException {
-    String translation = translateSourceFile(
-        "import java.util.*; class Test { static final Map<String, String> test = "
-        + "new HashMap<String, String>() {{ put(\"123\", \"123\"); }}; }", "Test", "Test.m");
-    assertTranslatedLines(translation,
-        "void Test_1_init(Test_1 *self) {",
-        "JavaUtilHashMap_init(self);",
-        "{",
-        "[self putWithId:@\"123\" withId:@\"123\"];",
-        "}",
-        "}");
+    String source =
+        """
+        import java.util.*;
+        class Test {
+          static final Map<String, String> test =
+              new HashMap<String, String>() {
+                {
+                  put("123", "123");
+                }
+              };
+        }
+        """;
+    String translation = translateSourceFile(source, "Test", "Test.m");
+    assertTranslatedLines(
+        translation,
+        """
+        void Test_1_init(Test_1 *self) {
+          JavaUtilHashMap_init(self);
+          {
+            [self putWithId:@"123" withId:@"123"];
+          }
+        }
+        """);
   }
 
   public void testConstantStringInitializationOrder() throws IOException {
-    String translation = translateSourceFile(
-        "class Test { private static final int CODE_POINT = getCodePoint(); "
-        + "private static final String FOO = \"\\uda6e\"; "
-        + "static int getCodePoint() { return FOO.codePointAt(0); } }", "Test", "Test.m");
+    String source =
+        """
+        class Test {
+          private static final int CODE_POINT = getCodePoint();
+          private static final String FOO = "\uda6e";
+          static int getCodePoint() {
+            return FOO.codePointAt(0);
+          }
+        }
+        """;
+    String translation = translateSourceFile(source, "Test", "Test.m");
     assertTranslatedLines(translation,
         "+ (void)initialize {",
         "  if (self == [Test class]) {",
@@ -286,8 +459,15 @@ public class InitializationNormalizerTest extends GenerationTest {
   // Verify that a static variable initialized as null doesn't do that in the
   // initialize method, since static variables are always zeroed in C.
   public void testNullInitializationIsStripped() throws IOException {
-    String translation = translateSourceFile(
-        "class test { private static Object CLASS_LOCK = null; }", "Test", "Test.m");
+    String translation =
+        translateSourceFile(
+            """
+            class test {
+              private static Object CLASS_LOCK = null;
+            }
+            """,
+            "Test",
+            "Test.m");
 
     // Is the null initializer still in the field declaration?
     assertNotInTranslation(translation, "static id test_CLASS_LOCK = nil;");
