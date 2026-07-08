d9a840a8018612d52aa1a3d9fcffc0a4090b564c
Wei Jun Kong <weijun@google.com>
2026-07-08 08:55:39 -0700

    Add swiftName attribute to ObjectiveCKmpMethod annotation
    PiperOrigin-RevId: 944527275

--- annotations/src/main/java/com/google/j2objc/annotations/ObjectiveCKmpMethod.java
+++ annotations/src/main/java/com/google/j2objc/annotations/ObjectiveCKmpMethod.java
@@ -80,4 +80,10 @@ public @interface ObjectiveCKmpMethod {
    * The class to map types on the method to Kotlin/Native interop types. This field is required.
    */
   Class<?> adapter();
+
+  /**
+   * The Swift name to use for the adapter method. Has the same naming requirements as the SwiftName
+   * annotation for methods.
+   */
+  String swiftName() default "";
 }
